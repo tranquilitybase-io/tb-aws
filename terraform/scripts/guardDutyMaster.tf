4b8dbd975459b8b297e48a8617e28e923747a9d9
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-12 13:18:31 -0600

    adding guardduty scripts for master and member accounts

---
+++ terraform/scripts/guardDutyMaster.tf
@@ -0,0 +1,14 @@
+resource "aws_guardduty_detector" "master" {
+  enable = true
+  finding_publishing_frequency = "FIFTEEN_MINUTES"
+}
+
+#Send invite
+resource "aws_guardduty_member" "memberInvite" {
+  count = "1"
+  detector_id = aws_guardduty_detector.master.id
+  account_id = "000000000000"
+  email = "some@example.com"
+  invite = true
+  invitation_message = "message" 
+}
