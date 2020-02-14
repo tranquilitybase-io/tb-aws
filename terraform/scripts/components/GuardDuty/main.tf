96da5d681dc0c3b8f363bdb661ddabeca7dc58fe
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-14 13:52:37 -0600

    member moved to main file, added variables file

---
+++ terraform/scripts/components/GuardDuty/main.tf
@@ -0,0 +1,28 @@
+#Enable GuardDuty on Master account
+resource "aws_guardduty_detector" "master" {
+  enable = true
+  finding_publishing_frequency = var.cloudwatch_notify_frecuency
+}
+
+#Send invite
+resource "aws_guardduty_member" "memberInvite" {
+  detector_id = aws_guardduty_detector.master.id
+  account_id = ["${data.aws_organizations_organization.all_accounts.id}"]
+  email = ["${data.aws_organizations_organization.all_accounts.id}"]
+  invite = true
+  invitation_message = var.member_invite_message 
+}
+
+#Accept invite
+resource "aws_guardduty_invite_accepter" "memberAccepter" {
+  count = var.auto_accept_invites == true ? 1 : 0
+  depends_on = [aws_guardduty_member.memberInvite]
+  detector_id = aws_guardduty_detector.member.id
+  master_account_id = aws_guardduty_detector.master.account_id
+}
+
+data "aws_organizations_organization" "all_accounts" {}
+
+output "account_ids" {
+  value = data.aws_organizations_organization.all_accounts.accounts[*].id
+}
--- terraform/scripts/components/GuardDuty/variables.tf
+++ terraform/scripts/components/GuardDuty/variables.tf
@@ -9,13 +9,14 @@ variable "cloudwatch_notify_frecuency" {
   }
 }
 
-variable "cloudwatch_notify_frecuency" {
+variable "member_invite_message" {
   type = string
-  description = "Frecuency that cloudwatch events will be notified. Accepted values: FIFTEEN_MINUTES, ONE_HOUR, SIX_HOURS."
-  default = "SIX_HOURS"
-  validation {
-    # regex(...) fails if it cannot find a match'
-    condition = var.cloudwatch_notify_frecuency == "FIFTEEN_MINUTES" || cloudwatch_notify_frecuency=="ONE_HOUR" || cloudwatch_notify_frecuency=="SIX_HOURS"
-    error_message = "The Notifications frecuency value in not valid."
-  }
+  description = "Message that the invite will display"
+  default = ""  
+}
+
+variable "auto_accept_invites" {
+  type = boolean
+  description = "Members will accept invties automatically (recommended)"
+  default = true 
 }
