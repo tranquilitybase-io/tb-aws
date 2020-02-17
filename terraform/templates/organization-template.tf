0231ba7c5f8033d87d2a6a65052cdfe03c99ea34
iota <ignacio.tissera@gft.com>
2020-02-17 15:45:14 -0600

    Adding parent to accounts for OU

--- terraform/templates/organization-template.tf
+++ terraform/templates/organization-template.tf
@@ -9,6 +9,7 @@ module "aws_lz_account_sharedservices" {
   org_account_name = var.aws_organizations_account_sharedservices_name
   org_account_email = var.aws_organizations_account_sharedservices_email
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
+  account_parent_id = module.aws_lz_ou_core.ou_id
 }
 
 module "aws_lz_account_logarchive" {
@@ -17,7 +18,7 @@ module "aws_lz_account_logarchive" {
   org_account_name = var.aws_organizations_account_logarchive_name
   org_account_email = var.aws_organizations_account_logarchive_email
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
-  
+  account_parent_id = module.aws_lz_ou_core.ou_id
 }
 
 module "aws_lz_account_security" {
@@ -26,7 +27,7 @@ module "aws_lz_account_security" {
   org_account_name = var.aws_organizations_account_security_name
   org_account_email = var.aws_organizations_account_security_email
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
-  
+  account_parent_id = module.aws_lz_ou_core.ou_id
 }
 module "aws_lz_ou_core" {
   source = "./templates/modules/organizations"
