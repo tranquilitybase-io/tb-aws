bf93f2ab37cbb4bee4560fc72941f6e12bbe673a
Deiver Kiel <deiver.kiel@gft.com>
2020-02-17 10:20:13 -0600

    update in gitlab-ci

--- automation/cicd/prepare.sh
+++ automation/cicd/prepare.sh
@@ -7,8 +7,13 @@ export TF_VAR_aws_secret_access_key=${secret_key}
 
 # Paths
 MAIN_PATH=$(pwd)
+AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform/scripts"
+
+${AUTOMATION_SCRIPTS}/terraform-pre-run.py
 
 cd ${TERRAFORM_PATH}
 terraform init
+terraform validate
+terraform refresh
 terraform plan
