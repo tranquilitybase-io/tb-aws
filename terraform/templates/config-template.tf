88654fe6f765f795a0cded75e73d301ee036489d
Sanchez Pacheco <cese@gft.com>
2020-02-18 14:01:11 -0600

    Set tags in config-service module

--- terraform/templates/config-template.tf
+++ terraform/templates/config-template.tf
@@ -32,6 +32,7 @@ module "aws_lz_config_service"{
   config_name = var.config_name
   config_logs_bucket = module.aws_lz_config_bucket.bucket_name_log
   config_logs_prefix = module.aws_lz_config_bucket.config_logs_prefix
+  config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.root_account_id, (var.tag_key_name) = "config" }
 }
 
 module "aws_lz_config_aggregator"{
