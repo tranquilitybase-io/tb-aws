d957509acd17fbd4a79d0156ab27af9bccd9f80a
Sanchez Pacheco <cese@gft.com>
2020-02-18 10:36:00 -0600

    Added account id in tags for Organization module

--- terraform/templates/organization-template.tf
+++ terraform/templates/organization-template.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  root_account_id = data.aws_caller_identity.current.account_id  
+}
+
 module "aws_lz_organization_main" {
   source = "./templates/modules/organizations"
   create_lz_organization = true 
@@ -8,7 +14,7 @@ module "aws_lz_account_sharedservices" {
   
   org_account_name = var.aws_organizations_account_sharedservices_name
   org_account_email = var.aws_organizations_account_sharedservices_email
-  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
+  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.root_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
 
@@ -17,7 +23,7 @@ module "aws_lz_account_logarchive" {
   
   org_account_name = var.aws_organizations_account_logarchive_name
   org_account_email = var.aws_organizations_account_logarchive_email
-  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
+  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.root_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
 
@@ -26,7 +32,7 @@ module "aws_lz_account_security" {
   
   org_account_name = var.aws_organizations_account_security_name
   org_account_email = var.aws_organizations_account_security_email
-  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = var.awslz_account_id, (var.tag_key_name) = "organization" }
+  org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.root_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
 module "aws_lz_ou_core" {
