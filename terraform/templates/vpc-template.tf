62f9cb9f39dae5f565ab19236dacd06f9ed5dedd
Deiver Kiel <deiver.kiel@gft.com>
2020-02-18 17:14:56 -0600

    Test using specific bucket

--- terraform/templates/vpc-template.tf
+++ terraform/templates/vpc-template.tf
@@ -2,3 +2,13 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "test My bucket"
+    Environment = "Dev"
+  }
+}
