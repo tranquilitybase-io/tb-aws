aecfeebc443b7db7ffc4d8cc1d7baec20d9edb78
Sanchez Pacheco <cese@gft.com>
2020-02-19 13:21:47 -0600

    Fixed service module

--- terraform/templates/modules/config/config-service/variables.tf
+++ terraform/templates/modules/config/config-service/variables.tf
@@ -1,27 +1,15 @@
-variable "config_name" {
-  default = ""
+
+variable "config_name" {  
+    default = ""
 }
 
-variable "bucket_name" {
-  description = "The S3 Bucket Name source for log bucket"
-  type        = string
-}
-
-variable "bucket_name_log" {
-  description = "The S3 Bucket Name for log."
-}
-
-variable "config_logs_prefix" {
-  description = "The S3 prefix for AWS Config logs."
+variable "config_tags" {
+  default = {} 
 }
 
 variable "role_arn" {
   description = "The ARN of role."
   type        = string
-}
-
-variable "config_tags" {
-  default = {} 
 }
 
 variable "config_delivery_frequency" {
