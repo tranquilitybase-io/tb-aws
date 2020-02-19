799213f6921d114cfc0d2759ec57a7d9eff7cbdb
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-19 11:07:39 -0600

    removing validation

--- terraform/templates/guardduty-variables.tf
+++ terraform/templates/guardduty-variables.tf
@@ -1,10 +1,11 @@
 variable "cloudwatch_notify_frecuency" {
   description = "Frecuency for CloudWatch Events to be notified. Accepted values: FIFTEEN_MINUTES, ONE_HOUR, SIX_HOURS."
   default = "SIX_HOURS"
-  validation {
-    condition = var.cloudwatch_notify_frecuency == "FIFTEEN_MINUTES" || var.cloudwatch_notify_frecuency=="ONE_HOUR" || var.cloudwatch_notify_frecuency=="SIX_HOURS"
-    error_message = "The Notifications frecuency value in not valid."
-  }
+ #POC: validation is experimental
+ # validation {
+ #   condition = var.cloudwatch_notify_frecuency == "FIFTEEN_MINUTES" || var.cloudwatch_notify_frecuency=="ONE_HOUR" || var.cloudwatch_notify_frecuency=="SIX_HOURS"
+ #   error_message = "The Notifications frecuency value in not valid."
+ # }
 }
 
 variable "invite_members"{
