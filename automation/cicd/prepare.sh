b307567d48285bfcf34e5cff97f2ea620a7d1c4e
Deiver Kiel <deiver.kiel@gft.com>
2020-02-19 00:46:24 -0600

    test all

--- automation/cicd/prepare.sh
+++ automation/cicd/prepare.sh
@@ -22,7 +22,8 @@ terraform init
 #terraform import module.aws_lz_organization_main.aws_organizations_organization.aws_lz_organization o-8lg1h3pzea
 #terraform import module.aws_lz_account_security.aws_organizations_account.aws_lz_account 971696596064
 #terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 371811507364
-terraform import module.aws_lz_account_sharedservices.aws_organizations_account.aws_lz_account 268856297607
+terraform import module.aws_lz_policy_tagging.aws_organizations_policy.aws_lz_policy p-95dn8ublc9
+
 cat main.tf
 echo "------------------------TERRAFORM VALIDATE----------------------------------------"
 terraform validate
