94ed5b52fd4fec04514598d1a130607c589f3fea
Sanchez Pacheco <cese@gft.com>
2020-02-20 13:42:40 -0600

    test

--- terraform/templates/modules/config/config-aggregator/main.tf
+++ terraform/templates/modules/config/config-aggregator/main.tf
@@ -16,16 +16,16 @@ data "aws_iam_policy_document" "aws_config_aggregator_role_policy" {
   }
 }
 
-resource "aws_iam_role" "aggregator" {
+resource "aws_iam_role" "aggregator_role" {
   count              = var.aggregate_organization ? 1 : 0
   name               = var.aggregator_role_name
   assume_role_policy = data.aws_iam_policy_document.aws_config_aggregator_role_policy.json
   tags = var.config_tags
 }
 
-resource "aws_iam_role_policy_attachment" "aggregator" {
+resource "aws_iam_role_policy_attachment" "aggregator_policy" {
   count      = var.aggregate_organization ? 1 : 0
-  role       = aws_iam_role.aggregator[0].name
+  role       = aws_iam_role.aggregator_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"  
 }
 
@@ -34,12 +34,12 @@ resource "aws_iam_role_policy_attachment" "aggregator" {
 #
 resource "aws_config_configuration_aggregator" "organization" {
   count      = var.aggregate_organization ? 1 : 0
-  depends_on = [aws_iam_role_policy_attachment.aggregator]
+  #depends_on = [aws_iam_role_policy_attachment.aggregator_policy]
   name       = var.aggregator_name
 
   organization_aggregation_source {
     all_regions = true
-    role_arn    = aws_iam_role.aggregator[0].arn
+    role_arn    = aws_iam_role.aggregator_role[0].arn
   }  
 }
 
