5388d927a574e39f3feb546ff81e4f98cd99b3d0
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-20 09:49:22 -0600

    cloudwatch_notify_frequency

--- terraform/templates/guardduty-template.tf
+++ terraform/templates/guardduty-template.tf
@@ -1,4 +1,4 @@
 module "aws_lz_guardduty_master" {
   source = "./templates/modules/guardduty"
-  cloudwatch_notify_frequency = "SIX_HOURS"
+  cloudwatch_notify_frequency = var.finding_publishing_frequency
 }
