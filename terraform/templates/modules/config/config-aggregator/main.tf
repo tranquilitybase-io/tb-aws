f665853909192fbb5d50924bc6b61a6e57a40205
Sanchez Pacheco <cese@gft.com>
2020-02-20 15:04:36 -0600

    Test aggregator

--- terraform/templates/modules/config/config-aggregator/main.tf
+++ terraform/templates/modules/config/config-aggregator/main.tf
@@ -18,14 +18,14 @@ data "aws_iam_policy_document" "aws_config_aggregator_role_policy" {
 
 resource "aws_iam_role" "aggregator" {
   count              = var.aggregate_organization ? 1 : 0
-  name               = var.aggregator_role_name
+  name               = "${config_name}-aggregator-role"
   assume_role_policy = data.aws_iam_policy_document.aws_config_aggregator_role_policy.json
   tags = var.config_tags
 }
 
 resource "aws_iam_role_policy_attachment" "aggregator" {
   count      = var.aggregate_organization ? 1 : 0
-  role       = aws_iam_role.aggregator.name
+  role       = aws_iam_role.aggregator[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"  
 }
 
@@ -38,7 +38,7 @@ resource "aws_config_configuration_aggregator" "organization" {
 
   organization_aggregation_source {
     all_regions = true
-    role_arn    = aws_iam_role.aggregator.arn
+    role_arn    = aws_iam_role.aggregator[0].arn
   }  
 }
 
