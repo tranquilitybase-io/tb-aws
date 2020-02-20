dc38fe8f072a17b62207be1393b6672f87f86848
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-20 09:08:54 -0600

    comment out finding_publishing_frequency

--- terraform/templates/guardduty-template.tf
+++ terraform/templates/guardduty-template.tf
@@ -1,6 +1,6 @@
 module "aws_lz_guardduty_master" {
   source = "./templates/modules/guardduty"
-  finding_publishing_frequency = var.cloudwatch_notify_frecuency
+ # finding_publishing_frequency = var.cloudwatch_notify_frecuency
  # invite = var.invite_members
  # invitation_message = var.member_invite_message 
 #  disable_email_notification =- var.disable_email_notification
