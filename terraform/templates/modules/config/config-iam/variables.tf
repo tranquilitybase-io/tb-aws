72241cb7c7432f24e1ea2bbdde962d186d0400cb
Sanchez Pacheco <cese@gft.com>
2020-02-21 15:05:05 -0600

    check IAM again

--- terraform/templates/modules/config/config-iam/variables.tf
+++ terraform/templates/modules/config/config-iam/variables.tf
@@ -1,10 +1,6 @@
 
-variable "config_logs_prefix" { 
+variable "config_logs_bucket" {    
     default = ""
-}
-
-variable "bucket_log_arn" {
-    description = "Bucket ARN of logging bucket"
 }
 
 variable "config_name" {  
@@ -15,27 +11,10 @@ variable "config_tags" {
   default = {} 
 }
 
-variable "bucket_account_id" {
-    description = "Account ID for Bucket creator"
+variable "config_logs_prefix" { 
     default = ""
 }
-/* 
-variable "iam_role_name" {
-    description = "Name of IAM role"
-    default = "$${config_name}_iam_role"
+
+variable "bucket_account_id" { 
+    description = "Log Archive Account ID"    
 }
-
- variable "aws_iam_policy_name" {
-    description = "Name of IAM policy"
-    default = "$${config_name}_policy"
-} 
-
-variable "iam_policy_attachment_name" {
-    description = "Name of Policy to be attached to IAM Role"
-    default = "$${config_name}_iam_policy_attachment"
-}
-
-variable "iam_managed_policy_attachment_name" {
-    description = "Name of Managed Policy to be attached to IAM Role"
-    default = "$${config_name}_iam_managed_policy_attachment"
-} */
