8963a669d27a17aae5895cd86498eda04f3095a3
iota <ignacio.tissera@gft.com>
2020-02-25 11:35:33 -0600

    Removing dups files after moving to diff folder

--- terraform/implementations/guardduty/guardduty-template.tf
+++
@@ -1,4 +0,0 @@
-module "aws_lz_guardduty_master" {
-  source = "./templates/modules/guardduty"
-  cloudwatch_notify_frequency = var.finding_publishing_frequency
-}
--- terraform/templates/modules/config/config-aggregator/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aggregator_name" {
-    value = concat(aws_config_configuration_aggregator.organization.*.name, [""])[0]
-}
