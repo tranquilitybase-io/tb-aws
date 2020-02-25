675ab2e0017bc8e6b91e40ebb6a5e716f498354b
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-02-25 10:39:55 -0600

    multiregion test

--- automation/cicd/terraform-pre-run.py
+++ automation/cicd/terraform-pre-run.py
@@ -70,8 +70,8 @@ def merge_files():
                                         if line != first_line and line != second_line:
                                             fout.write(line)
                         else:
-                            print (finput.read())
-                            fout.write(finput.read())
+                           for line in lines:
+                               fout.write(line)
                     fout.write(f'\n ####### END FILE {file_name} #####  \n')
                     finput.close()
                 else:
