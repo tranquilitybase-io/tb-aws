5b3a9cb298831b48a7a8d30666e6e9a071f224b4
Sanchez Pacheco <cese@gft.com>
2020-02-26 10:17:43 -0600

    cloudtrail agaian

--- terraform/modules/cloudtrail/main.tf
+++ terraform/modules/cloudtrail/main.tf
@@ -12,8 +12,6 @@ resource "aws_cloudtrail" "cloudtrail_default" {
   tags                       = var.required_tags
 }
 
-Error: Cycle: module.aws_lz_cloudtrail.data.aws_iam_policy_document.cloudtrail_alarm_policy, module.aws_lz_cloudtrail.aws_sns_topic.sns_topic_default
-
 resource "aws_cloudwatch_log_group" "log_group_default" {
   name = "${var.cloudtrail_name}_${var.cloudwatch_log_group}"
   retention_in_days = var.logs_retencion_days
