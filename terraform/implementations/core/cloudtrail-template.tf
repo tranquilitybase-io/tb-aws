e10761805aaa405837fecf7709d8a4e68e4e59c9
Sanchez Pacheco <cese@gft.com>
2020-02-27 11:38:24 -0600

    check topic

--- terraform/implementations/core/cloudtrail-template.tf
+++ terraform/implementations/core/cloudtrail-template.tf
@@ -7,7 +7,7 @@ module "aws_lz_sns_topic" {
   required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "cloudtrail" }  
 }
 
-module "aws_lz_cloudtrail" {
+/* module "aws_lz_cloudtrail" {
   source = "./modules/cloudtrail"  
   cloudtrail_name = var.cloudtrail_name
   bucket_name = module.aws_lz_config_bucket.bucket_name_log
@@ -17,6 +17,5 @@ module "aws_lz_cloudtrail" {
   sns_topic_arn = module.aws_lz_sns_topic.topic_arn
   s3_log_prefix = module.aws_lz_config_bucket.s3_log_prefix
   region = data.aws_region.current.name
-  required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "cloudtrail" }  
-  depends_on = [module.aws_lz_sns_topic]
-}
+  required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "cloudtrail" }    
+} */
