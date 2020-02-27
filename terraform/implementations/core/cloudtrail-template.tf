5af919a9e7665090ea909ce28902002795c9a0dc
Sanchez Pacheco <cese@gft.com>
2020-02-27 11:34:38 -0600

    check depends_on

--- terraform/implementations/core/cloudtrail-template.tf
+++ terraform/implementations/core/cloudtrail-template.tf
@@ -8,7 +8,7 @@ module "aws_lz_sns_topic" {
 }
 
 module "aws_lz_cloudtrail" {
-  source = "./modules/cloudtrail"
+  source = "./modules/cloudtrail"  
   cloudtrail_name = var.cloudtrail_name
   bucket_name = module.aws_lz_config_bucket.bucket_name_log
   bucket_arn = module.aws_lz_config_bucket.bucket_log_arn
@@ -18,4 +18,5 @@ module "aws_lz_cloudtrail" {
   s3_log_prefix = module.aws_lz_config_bucket.s3_log_prefix
   region = data.aws_region.current.name
   required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "cloudtrail" }  
+  depends_on = [module.aws_lz_sns_topic]
 }
