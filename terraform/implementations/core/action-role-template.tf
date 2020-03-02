7b2761fdad186a55f4c0e850e9a1d7f463c6e842
iota <ignacio.tissera@gft.com>
2020-03-02 15:27:02 -0600

    Removing config temporary for concile TF status

--- terraform/implementations/core/action-role-template.tf
+++ terraform/implementations/core/action-role-template.tf
@@ -135,34 +135,7 @@ data "aws_iam_policy_document" "aws_lz_role_policy_network"{
     }
 }
 
-/*module "aws_lz_iam_role_security" {
-    source = "./modules/iam"
-    name = var.security_role_name
-    assume_role_policy = data.aws_iam_policy_document.aws_lz_assume_role_security.json
-    role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "action-role" }
-}
-
-module "aws_lz_iam_role_logarchive" {
-    source = "./modules/iam"
-    name = var.logarchive_role_name
-    assume_role_policy = data.aws_iam_policy_document.aws_lz_assume_role_logarchive.json
-    role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "action-role" }
-}
-
-module "aws_lz_iam_role_sharedservices" {
-    source = "./modules/iam"
-    name = var.sharedservices_role_name
-    assume_role_policy = data.aws_iam_policy_document.aws_lz_assume_role_sharedservices.json
-    role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "action-role" }
-}
-
-module "aws_lz_iam_role_network" {
-    source = "./modules/iam"
-    name = var.network_role_name
-    assume_role_policy = data.aws_iam_policy_document.aws_lz_assume_role_network.json
-    role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "action-role" }
-}
-
+/*
 module "aws_lz_iam_policy_security" {
     source = "./modules/iam"
     policy_name = "SecurityPolicy"
