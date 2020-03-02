d9e60b560fdc73fa3e2c232172d392d33a16019f
iota <ignacio.tissera@gft.com>
2020-03-02 14:26:29 -0600

    Testing assume role new role

--- terraform/implementations/core/organization-template.tf
+++ terraform/implementations/core/organization-template.tf
@@ -37,7 +37,7 @@ module "aws_lz_account_security" {
   
   org_account_name = var.aws_organizations_account_security_name
   org_account_email = var.aws_organizations_account_security_email
-  account_role_name = var.org_admin_role
+  #account_role_name = var.org_admin_role
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
@@ -47,7 +47,7 @@ module "aws_lz_account_logarchive" {
   
   org_account_name = var.aws_organizations_account_logarchive_name
   org_account_email = var.aws_organizations_account_logarchive_email
-  account_role_name = var.org_admin_role
+  #account_role_name = var.org_admin_role
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
@@ -57,7 +57,7 @@ module "aws_lz_account_sharedservices" {
   
   org_account_name = var.aws_organizations_account_sharedservices_name
   org_account_email = var.aws_organizations_account_sharedservices_email
-  account_role_name = var.org_admin_role
+  #account_role_name = var.org_admin_role
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
@@ -67,7 +67,7 @@ module "aws_lz_account_network" {
   
   org_account_name = var.aws_organizations_account_network_name
   org_account_email = var.aws_organizations_account_network_email
-  account_role_name = var.org_admin_role
+  #account_role_name = var.org_admin_role
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
 }
