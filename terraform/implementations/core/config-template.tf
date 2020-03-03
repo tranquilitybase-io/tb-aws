3e8382631b88f9ff438ebdb28d7714a253b8df1f
Sanchez Pacheco <cese@gft.com>
2020-03-03 16:19:52 -0600

    check topic arn

--- terraform/implementations/core/config-template.tf
+++ terraform/implementations/core/config-template.tf
@@ -56,7 +56,7 @@ module "aws_lz_config_service" {
   config_name = var.config_name
   config_logs_bucket = module.aws_lz_config_bucket.bucket_name_log
   s3_log_prefix = var.s3_log_prefix
-  topic_account_id = local.current_account_id
+  topic_account_id = local.log_archive_account_id
   sns_topic_arn = module.aws_lz_config_sns_topic.topic_arn
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.log_archive_account_id, (var.tag_key_name) = "config" }
 }
