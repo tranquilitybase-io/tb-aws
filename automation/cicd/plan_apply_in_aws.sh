24ee3efa7abe941ea4b1b261ecba20e1492e86e9
Deiver Kiel <deiver.kiel@gft.com>
2020-03-03 11:21:06 -0600

    Test add log acc and apply

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -26,7 +26,7 @@ echo "------------------------TERRAFORM REMOVE ACCOUNTS FROM STATE END----------
 #terraform import module.aws_lz_account_network.aws_organizations_account.aws_lz_account 615513573213 
 #terraform import module.aws_lz_account_security.aws_organizations_account.aws_lz_account 971696596064
 #terraform import module.aws_lz_account_sharedservices.aws_organizations_account.aws_lz_account 000516684594
-#terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 543433398002
+terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 543433398002
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
 terraform apply -auto-approve
