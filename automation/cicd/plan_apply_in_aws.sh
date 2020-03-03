e4a20a1b9a73f6a07b55d3b0d59e2f0c6724d4bd
Deiver Kiel <deiver.kiel@gft.com>
2020-03-03 11:37:53 -0600

    clean apply script

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -12,21 +12,6 @@ TERRAFORM_PATH="${MAIN_PATH}/terraform"
 cd ${TERRAFORM_PATH}
 echo "------------------------TERRAFORM INIT--------------------------------------------"
 terraform init
-#terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 543433398002
-#terraform refresh
-echo "------------------------TERRAFORM REMOVE ACCOUNTS FROM STATE--------------------------------------------"
-#terraform state rm module.aws_lz_account_security.aws_organizations_account.aws_lz_account
-#terraform state rm module.aws_lz_account_sharedservices.aws_organizations_account.aws_lz_account
-#terraform state rm module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account
-#terraform state rm module.aws_lz_account_network.aws_organizations_account.aws_lz_account
-echo "------------------------TERRAFORM REMOVE ACCOUNTS FROM STATE END--------------------------------------------"
-#terraform import module.aws_lz_account_security.aws_organizations_account.aws_lz_account 971696596064
-#terraform import module.aws_lz_account_sharedservices.aws_organizations_account.aws_lz_account 000516684594
-#terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 705215869482
-#terraform import module.aws_lz_account_network.aws_organizations_account.aws_lz_account 615513573213 
-#terraform import module.aws_lz_account_security.aws_organizations_account.aws_lz_account 971696596064
-#terraform import module.aws_lz_account_sharedservices.aws_organizations_account.aws_lz_account 000516684594
-#terraform import module.aws_lz_account_logarchive.aws_organizations_account.aws_lz_account 543433398002
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
 terraform apply -auto-approve
