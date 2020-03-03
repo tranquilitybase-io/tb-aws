5b458d897399b3a82f1260ec19066bd62dd60176
Sanchez Pacheco <cese@gft.com>
2020-03-03 14:36:13 -0600

    Check Config service

--- terraform/modules/config/config-service/main.tf
+++ terraform/modules/config/config-service/main.tf
@@ -5,7 +5,20 @@
 resource "aws_config_configuration_recorder_status" "main" {
   name       = "${var.config_name}_${var.recorder_name}"
   is_enabled = true
-  depends_on = [aws_config_delivery_channel.main]
+  #depends_on = [aws_config_delivery_channel.main]
+}
+
+resource "aws_config_delivery_channel" "main" {
+  name           = var.aws_config_delivery_channel_name
+  s3_bucket_name = var.config_logs_bucket
+  s3_key_prefix  = var.s3_log_prefix
+
+  sns_topic_arn = aws_sns_topic.config_sns_topic.arn
+
+  snapshot_delivery_properties {
+    delivery_frequency = var.config_delivery_frequency
+  }
+  depends_on = [aws_config_configuration_recorder.main]
 }
 
 resource "aws_config_configuration_recorder" "main" {
@@ -24,17 +37,4 @@ resource "aws_sns_topic" "config_sns_topic" {
   tags = var.config_tags
 }
 
-
-resource "aws_config_delivery_channel" "main" {
-  name           = var.aws_config_delivery_channel_name
-  s3_bucket_name = var.config_logs_bucket
-  s3_key_prefix  = var.s3_log_prefix
-
-  sns_topic_arn = aws_sns_topic.config_sns_topic.arn
-
-  snapshot_delivery_properties {
-    delivery_frequency = var.config_delivery_frequency
-  }
-  depends_on = [aws_config_configuration_recorder.main]
-}
 
