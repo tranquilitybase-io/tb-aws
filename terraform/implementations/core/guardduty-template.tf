e684ffd008eb79419091ce577d580778920a4d59
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 14:42:42 -0600

    S3 KMS

--- terraform/implementations/core/guardduty-template.tf
+++ terraform/implementations/core/guardduty-template.tf
@@ -20,7 +20,6 @@ module "aws_lz_guardduty_bucket" {
   providers = {
     aws = aws.logarchive-account
   }
-  sse_algorithm = var.algorithm
   kms_key = module.aws_lz_finding_bucket_key.key_id
   bucket_name = local.bucket_name_findings
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
