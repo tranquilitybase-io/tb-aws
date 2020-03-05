4f1730df6e7fe92ea367e36d0d427232545918ca
Deiver Kiel <deiver.kiel@gft.com>
2020-03-05 10:27:34 -0600

    test module

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami           = var.aws_ec2_ami
+  instance_type = var.aws_ec2_instance_type #var.aws_ec2_instance_type
+
+  tags = {
+    Name = "HelloWorld2"
+  }
+}
---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_ec2_ami" {
+    type    = string
+    default = ""
+}
+
+variable "aws_ec2_instance_type" {
+    type    = string
+    default = ""
+}
