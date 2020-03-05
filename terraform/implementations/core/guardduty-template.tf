c8bcec5b7e10b4d55d7514ba1566915ad1cb5ebe
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 10:50:54 -0600

    test bucket creation

--- terraform/implementations/core/guardduty-template.tf
+++ terraform/implementations/core/guardduty-template.tf
@@ -1,7 +1,7 @@
 locals {
   #log_archive_account_id = module.aws_lz_account_logarchive.account_id
-  region = data.aws_region.current.name
-  bucket_name = "aws-lz-s3-guardduty-findings" 
+  region_findings = data.aws_region.current.name
+  bucket_name_findings = "aws-lz-s3-guardduty-findings" 
 }
 
 module "aws_lz_guardduty_bucket" {
@@ -10,7 +10,7 @@ module "aws_lz_guardduty_bucket" {
   providers = {
     aws = aws.logarchive-account
   }
-
-  bucket_name = local.bucket_name
+  region = locals.region_findings
+  bucket_name = local.bucket_name_findings
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
 }
--- terraform/modules/guardduty/guardduty-s3-bucket/variables.tf
+++ terraform/modules/guardduty/guardduty-s3-bucket/variables.tf
@@ -1,25 +1,25 @@
 
-variable "findings_bucket_name" {
+variable "bucket_name" {
   description = "The S3 Bucket Name source for findings bucket"
   type        = string
 }
 
-variable "findings_config_tags" {
+variable "config_tags" {
   default = {} 
 }
 
-variable "findings_versioning_enabled"{
+variable "versioning_enabled"{
   description = "Set versioning in S3 bucket"
   type        = bool
   default = true
 }
 
-variable "findings_acl_access_bucket" {
+variable "acl_access_bucket" {
   description = "ACL value for Access Bucket"
   default = "log-delivery-write"
 }
 
-variable "findings_sse_aes256" {
+variable "sse_aes256" {
   description = "Encryption AES256"
   default = "AES256"
 }
