7da63ff86715344cf872ce727dc10c7a643d4d2a
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 09:59:33 -0600

    test

--- automation/cicd/guardduty.py
+++ automation/cicd/guardduty.py
@@ -27,7 +27,7 @@ def get_accounts_data():
             account_list.append(Account(member["Name"],member["Id"],member["Email"],[]))   
     return account_list
 #Get enabled regions
-def get_regions():
+def get_regions(): 
     regions_list = {}
     enable_regions_prefix = {"us","eu","ca","sa"}
     client = boto3.client('ec2')
