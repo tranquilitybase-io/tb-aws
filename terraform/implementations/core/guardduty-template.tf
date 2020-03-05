e41d03ce87a2a1f9ab7d7fa934a660399e5e8d90
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 11:17:12 -0600

    test bucket

--- terraform/implementations/core/guardduty-template.tf
+++ terraform/implementations/core/guardduty-template.tf
@@ -1,4 +1,4 @@
-locals {
+/* locals {
   #log_archive_account_id = module.aws_lz_account_logarchive.account_id
   region_findings = data.aws_region.current.name
   bucket_name_findings = "aws-lz-s3-guardduty-findings" 
@@ -12,4 +12,22 @@ module "aws_lz_guardduty_bucket" {
  
   bucket_name = local.bucket_name_findings
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
+} */
+
+
+resource "aws_s3_bucket" "s3_findings" {
+  bucket = "findings"
+  acl    = "log-delivery-write"
+  versioning {
+    enabled = true
+  }
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = var.config_tags
 }
