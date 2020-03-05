60b340e74f5c32be6fd08df8c4d30d8a194bdf0a
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 11:06:45 -0600

    test

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -4,13 +4,15 @@
 export AWS_ACCESS_KEY_ID=${access_key}
 export AWS_SECRET_ACCESS_KEY=${secret_key}
 export AWS_DEFAULT_REGION=${DEV_region}
- 
+
 # Paths 
 MAIN_PATH=$(pwd)
 AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform"
 
 cd ${TERRAFORM_PATH}
+echo "-----------------------PRINTING MAIN.TF-------------------------------------------"
+cat main.tf 
 echo "------------------------TERRAFORM INIT--------------------------------------------"
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
