a3412db1234e07dde906a7bf91426f6c996dc450
Deiver Kiel <deiver.kiel@gft.com>
2020-03-06 11:12:33 -0600

    test remove buckets

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -16,10 +16,11 @@ echo "------------------------TERRAFORM INIT------------------------------------
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
-terraform apply -auto-approve 
+#terraform apply -auto-approve 
 
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
 
 #terraform state rm module.aws_lz_config_service.aws_sns_topic_policy.sns_default_policy
-#terraform state rm module.aws_lz_config_sns_topic.aws_sns_topic.sns_topic_default
+terraform state rm module.aws_lz_config_bucket.aws_s3_bucket.s3_main
+terraform state rm module.aws_lz_config_bucket.aws_s3_bucket.s3_log
