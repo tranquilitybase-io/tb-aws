9a418501d92afcabf87c76324d4220b79a72150f
Deiver Kiel <deiver.kiel@gft.com>
2020-03-06 14:18:21 -0600

    Test comment bucket

--- terraform/implementations/core/guardduty-template.tf
+++ terraform/implementations/core/guardduty-template.tf
@@ -13,6 +13,7 @@ module "aws_lz_finding_bucket_key" {
   kms_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
 }
 
+/*
 module "aws_lz_guardduty_bucket" {
   source = "./modules/s3/"
   
@@ -23,6 +24,7 @@ module "aws_lz_guardduty_bucket" {
   bucket_name = local.bucket_name_findings
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
 }
+*/
 /*
 module "guardduty_s3_policy" {
   source = "./modules/guardduty/bucket_policy"
