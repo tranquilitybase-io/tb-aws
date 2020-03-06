59f0161a1aa13b1db0fe608c2a77f0e8c0bb5251
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-05 19:28:43 -0600

    Test key permissions

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -6,7 +6,7 @@ resource "aws_kms_key" "kms_key" {
 
 resource "aws_kms_grant" "guardduty_key_permission" {
   name = var.key_permission_name
-  key_id = "${aws_kms_key.kms_key.key_id}"
+  key_id = aws_kms_key.kms_key.key_id
   grantee_principal = var.key_permission_principal
   operations = var.key_permission_operations
 }
