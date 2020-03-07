8fd4ec3a88cd58ae119fbcc980d6c02dd59356df
csanchez <Github1304>
2020-03-06 20:40:04 -0600

    Config in Log Archive again

--- terraform/modules/config/config-service/main.tf
+++ terraform/modules/config/config-service/main.tf
@@ -1,23 +1,14 @@
 #
 # AWS Config Service
 #
-resource "aws_config_configuration_recorder" "main" {
-  name     = "${var.config_name}_recorder"
-  role_arn = aws_iam_role.main.arn
-
-  recording_group {
-    all_supported                 = true
-    include_global_resource_types = true
-  }
-}
-
 resource "aws_config_configuration_recorder_status" "main" {
-  name       = aws_config_configuration_recorder.main.name
+  name       = "${var.config_name}_recorder"
   is_enabled = true
+  depends_on = [aws_config_delivery_channel.main]
 }
 
 resource "aws_config_delivery_channel" "main" {
-  name           = aws_config_configuration_recorder.main.name
+  name           = "${var.config_name}_recorder"
   s3_bucket_name = var.config_logs_bucket
   s3_key_prefix  = var.s3_log_prefix
 
@@ -26,7 +17,17 @@ resource "aws_config_delivery_channel" "main" {
   snapshot_delivery_properties {
     delivery_frequency = var.config_delivery_frequency
   }
-  #depends_on = [aws_config_configuration_recorder.main]
+  depends_on = [aws_config_configuration_recorder.main]
+}
+
+resource "aws_config_configuration_recorder" "main" {
+  name     = "${var.config_name}_recorder"
+  role_arn = aws_iam_role.main.arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = true
+  }
 }
 
 resource "aws_iam_role" "main" {
@@ -47,7 +48,7 @@ resource "aws_iam_policy" "aws_config_policy" {
 }
 
 resource "aws_iam_policy_attachment" "aws_config_policy" {
-  name       = aws_iam_policy.aws_config_policy.name
+  name       = "${var.config_name}_iam_policy"
   roles      = [aws_iam_role.main.name]
   policy_arn = aws_iam_policy.aws_config_policy.arn
 }
