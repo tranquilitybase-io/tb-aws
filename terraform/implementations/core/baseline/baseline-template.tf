e7d09a13b0480f03699c0ad7a0ce6ace95d77c9a
csanchez <Github1304>
2020-03-07 00:04:11 -0600

    Config in Sandbox account

--- terraform/implementations/core/baseline/baseline-template.tf
+++ terraform/implementations/core/baseline/baseline-template.tf
@@ -1,5 +1,4 @@
 
-
 module "aws_lz_config_sns_topic" {
   source = "./modules/snstopic"
 
@@ -18,11 +17,29 @@ module "aws_lz_config_service" {
     aws = aws.sandbox-account
   }
 
-  config_name            = var.config_name
-  config_logs_bucket     = module.aws_lz_config_bucket.bucket_name_log
-  s3_log_prefix          = module.aws_lz_config_bucket.s3_log_prefix
-  topic_account_id       = local.sandbox_account_id
-  sns_topic_arn          = module.aws_lz_config_sns_topic.topic_arn
-  config_tags            = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
+  config_name        = var.config_name
+  config_logs_bucket = module.aws_lz_config_bucket.bucket_name_log
+  s3_log_prefix      = module.aws_lz_config_bucket.s3_log_prefix
+  topic_account_id   = local.sandbox_account_id
+  sns_topic_arn      = module.aws_lz_config_sns_topic.topic_arn
+  config_tags        = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
 }
 
+module "aws_lz_config_aggregator" {
+  source = "./modules/config/config-aggregator"
+
+  providers = {
+    aws = aws.sandbox-account
+  }
+
+  config_name              = var.config_name
+  region                   = local.region
+  authorization_account_id = local.sandbox_account_id
+  config_tags              = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
+}
+
+module "aws_lz_config_rules" {
+  source           = "./modules/config/config-rules"
+  recorder_main    = module.aws_lz_config_service.recorder_main
+  delivery_channel = module.aws_lz_config_service.delivery_channel
+}
--- terraform/implementations/core/logarchive/logarchive-template.tf
+++ terraform/implementations/core/logarchive/logarchive-template.tf
@@ -3,50 +3,6 @@ locals {
   bucket_name_log = "aws-lz-s3-logs-${local.logarchive_account_id}-${local.region}"  
 }
 
-data "template_file" "logarchive_bucket_policy" {
-  template = <<JSON
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Sid": "AWSConfigBucketPermissionsCheck",
-        "Effect": "Allow",
-        "Principal" : {
-          "Service" : [
-            "cloudtrail.amazonaws.com",
-            "config.amazonaws.com"
-          ]
-        },
-        "Action": "s3:GetBucketAcl",
-        "Resource": "${module.aws_lz_config_bucket.bucket_log_arn}"
-    },
-    {
-        "Sid": "AWSConfigBucketDelivery",
-        "Effect": "Allow",
-        "Principal" : {
-          "Service" : [
-            "cloudtrail.amazonaws.com",
-            "config.amazonaws.com"
-          ]
-        },
-        "Action": "s3:PutObject",
-        "Resource": ["$${config_resource}","$${cloudtrail_resource}"],
-        "Condition": {
-          "StringLike": {
-            "s3:x-amz-acl": "bucket-owner-full-control"
-          }
-        }
-    }
-  ]
-}
-JSON
-
-  vars = {
-    config_resource = format("%s/%s/AWSLogs/%s/Config/*",module.aws_lz_config_bucket.bucket_log_arn,module.aws_lz_config_bucket.s3_log_prefix,local.sandbox_account_id)
-    cloudtrail_resource = format("%s/%s/AWSLogs/%s/CloudTrail/*",module.aws_lz_config_bucket.bucket_log_arn,module.aws_lz_config_bucket.s3_log_prefix,local.sandbox_account_id)
-  }
-}
-
 module "aws_lz_config_bucket" {
   source = "./modules/config/config-s3-bucket"
 
@@ -59,17 +15,6 @@ module "aws_lz_config_bucket" {
     aws = aws.logarchive-account
   }
 }
-/*
-module "aws_s3_bucket_policy_logarchive"{
-    source = "./modules/s3"
-    
-    bucket_name = module.aws_lz_config_bucket.bucket_name_log
-    s3_bucket_policy = data.template_file.logarchive_bucket_policy.rendered
-
-    providers = {
-      aws = aws.logarchive-account
-    }
-}*/
 
 
 
