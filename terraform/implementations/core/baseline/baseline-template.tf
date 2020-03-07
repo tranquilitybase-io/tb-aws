0404160982c5cb7aab5e1bba34f1a823a5f11a19
csanchez <Github1304>
2020-03-06 20:11:49 -0600

    Config in Log Archive

--- terraform/implementations/core/baseline/baseline-template.tf
+++ terraform/implementations/core/baseline/baseline-template.tf
@@ -22,7 +22,7 @@ module "aws_lz_config_service" {
   config_logs_bucket = module.aws_lz_config_bucket.bucket_name_log
   config_logs_bucket_arn = module.aws_lz_config_bucket.bucket_log_arn
   s3_log_prefix = module.aws_lz_config_bucket.s3_log_prefix
-  topic_account_id = module.aws_lz_account_sandbox.account_id
+  topic_account_id = module.aws_lz_account_logarchive.account_id
   sns_topic_arn = module.aws_lz_config_sns_topic.topic_arn
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_account_sandbox.account_id, (var.tag_key_name) = "config" }
 }
