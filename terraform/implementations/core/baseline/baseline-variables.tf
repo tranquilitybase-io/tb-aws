3c2db1e79fc3345fc8a7f0d5e1926f8ea3d33851
csanchez <Github1304>
2020-03-09 14:48:21 -0600

    Cloudtrail in SandBox account

--- terraform/implementations/core/baseline/baseline-variables.tf
+++ terraform/implementations/core/baseline/baseline-variables.tf
@@ -4,6 +4,11 @@ variable "config_name" {
     description = "Config Service name"
 }
 
+variable "cloudtrail_name" {
+  default = "aws_lz_cloudtrail"
+  description = "Config Service name"
+}
+
 variable "local_topic_name" {
   default = "aws_lz_local_sns_topic"
   description = "Name of topic for Config service"
