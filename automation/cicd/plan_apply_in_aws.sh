bb280e8a69dc4351022646edbd7bedcb4d2452ae
csanchez <Github1304>
2020-03-09 17:58:01 -0600

    Cloudtrail in SandBox account

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -16,12 +16,12 @@ echo "------------------------TERRAFORM INIT------------------------------------
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
-terraform apply -auto-approve
+#terraform apply -auto-approve
 
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
 
-#terraform state rm module.aws_lz_config_service.aws_sns_topic_policy.sns_default_policy
+terraform state rm module.aws_lz_cloudtrail.aws_sns_topic_policy.sns_default_policy
 #terraform state rm module.aws_lz_config_bucket.aws_s3_bucket.s3_main
 #terraform state rm module.aws_lz_config_bucket.aws_s3_bucket.s3_log
 
