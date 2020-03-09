a79ca63a28f77cb348966ef255258cf192f5ab5a
Deiver Kiel <deiver.kiel@gft.com>
2020-03-09 12:13:16 -0600

    test create bucket

--- terraform/implementations/core/guardduty-template.tf
+++ terraform/implementations/core/guardduty-template.tf
@@ -1,6 +1,6 @@
 locals {
   region_findings = data.aws_region.current.name
-  bucket_name_findings = "aws-lz-s3-guardduty-findings-${local.current_account_id}-${local.region}"
+  bucket_name_findings = "aws-lz-s3-guardduty-findings-${local.logarchive_account_id}-${local.region}"
 }
 
 module "aws_lz_finding_bucket_key" {
@@ -13,7 +13,7 @@ module "aws_lz_finding_bucket_key" {
   kms_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
 }
 
-/*
+
 module "aws_lz_guardduty_bucket" {
   source = "./modules/s3/"
   
@@ -24,7 +24,7 @@ module "aws_lz_guardduty_bucket" {
   bucket_name = local.bucket_name_findings
   config_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "config" }
 }
-*/
+
 /*
 module "guardduty_s3_policy" {
   source = "./modules/guardduty/bucket_policy"
