559dc956f748670802e61e4891043d483e54f0cd
cese <cese@gft.com>
2020-03-09 20:57:35 -0600

    Topic Policy

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -7,6 +7,7 @@ module "aws_lz_aggregate_security_sns_topic" {
   } 
 
   sns_topic_name = var.aggregate_topic_name
+  topic_account_id = local.security_account_id
   required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_account_security.account_id, (var.tag_key_name) = "Aggregate Security" }
 }
 
