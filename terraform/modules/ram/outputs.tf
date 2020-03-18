5f0f65ec3a72a971e7f5f5dd8d8728fabb70d9de
iota <ignacio.tissera@gft.com>
2020-03-18 08:30:35 -0600

    Adding module for AWS RAM

---
+++ terraform/modules/ram/outputs.tf
@@ -0,0 +1,9 @@
+output "ram_resource_share_arn" {
+  description = "The Amazon Resource Name (ARN) of the resource share"
+  value       = element(concat(aws_ram_resource_share.aws_lz_ram_shared.*.id, [""]), 0)
+}
+
+output "ram_principal_association_arn" {
+  description = "The Amazon Resource Name (ARN) of the Resource Share and the principal, separated by a comma"
+  value       = element(concat(aws_ram_principal_association.aws_lz_ram_principal_association.*.id, [""]), 0)
+}
