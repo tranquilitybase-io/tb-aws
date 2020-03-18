a8b478de6d8cc63b4e3c0b78f2cda2a5a8724dd0
cese <cese@gft.com>
2020-03-18 11:29:27 -0600

    TGW + Egress VPC

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -16,13 +16,13 @@ echo "------------------------TERRAFORM INIT------------------------------------
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
-#terraform apply -auto-approve
+terraform apply -auto-approve
 
 
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
 
-terraform state rm module.vpc-network-account
+#terraform state rm module.vpc-network-account
 #terraform state rm module.aws_lz_config_sns_topic
 
 #terraform state rm module.aws_lz_config_bucket.aws_s3_bucket.s3_main
