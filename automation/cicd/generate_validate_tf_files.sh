dc3992030342bbe69a8d374c1e028c155ceb8456
Deiver Kiel <deiver.kiel@gft.com>
2020-03-18 00:20:17 -0600

    test before and after replace

--- automation/cicd/generate_validate_tf_files.sh
+++ automation/cicd/generate_validate_tf_files.sh
@@ -15,6 +15,15 @@ TERRAFORM_PATH="${MAIN_PATH}/terraform"
 python3 ${AUTOMATION_SCRIPTS}/terraform-pre-run.py
 
 cd ${TERRAFORM_PATH}
+echo "Before replace---------------------------------------"
+cat implementations/core/organization-variables.tf
+sed -i "s;sharedservice_email;tl93372@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;logarchive_email;tl93373@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;security_email;test.gft.aws.03@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;network_email;test.gft.aws.04@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account_email;tb.test.logging.01@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account-2_email;tb.test.logging.02@gmail.com;" implementations/core/organization-variables.tf
+echo "After replace---------------------------------------"
 cat implementations/core/organization-variables.tf
 echo "-----------------------PRINTING MAIN.TF-------------------------------------------"
 cat main.tf 
