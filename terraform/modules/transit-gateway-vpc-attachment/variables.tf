82b6571d5d3b0face273ab9a139fd0c1f49a20cd
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-18 13:57:35 -0600

    test | tgw-vpc attachment

--- terraform/modules/transit-gateway-vpc-attachment/variables.tf
+++ terraform/modules/transit-gateway-vpc-attachment/variables.tf
@@ -1,18 +1,15 @@
 variable "subnets_ids"{
     description= "Identifiers of subnets"
     type = list
-    default = []
 }
 
 variable "transit_gateway_id"{
     description= "Transit gateway Id"
     type = string
-    default = ""
 }
 
 
 variable "vpc_id"{
     description= "vpc_id"
     type = string
-    default = ""
 }
