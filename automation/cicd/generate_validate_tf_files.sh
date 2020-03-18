ef494a968cf45f1361688a141f6a48804dfb9ab8
Deiver Kiel <deiver.kiel@gft.com>
2020-03-18 01:02:30 -0600

    test 7

--- automation/cicd/generate_validate_tf_files.sh
+++ automation/cicd/generate_validate_tf_files.sh
@@ -11,25 +11,10 @@ MAIN_PATH=$(pwd)
 AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform"
 
-
-echo "Before replacement---------------------"
-cat implementations/core/organization-variables.tf
-sed -i "s;sharedservice_email;tl93372@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-sed -i "s;logarchive_email;tl93373@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-sed -i "s;security_email;test.gft.aws.03@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-sed -i "s;network_email;test.gft.aws.04@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-sed -i "s;test-account_email;tb.test.logging.01@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-sed -i "s;test-account-2_email;tb.test.logging.02@gmail.com;" ${TERRAFORM_PATH}/implementations/core/organization-variables.tf
-echo "After replacement---------------------"
-cat implementations/core/organization-variables.tf
-
 # Files preparation script
 python3 ${AUTOMATION_SCRIPTS}/terraform-pre-run.py
 
 cd ${TERRAFORM_PATH}
-
-echo "After terraform pre run---------------------"
-cat implementations/core/organization-variables.tf
 
 echo "-----------------------PRINTING MAIN.TF-------------------------------------------"
 cat main.tf 
