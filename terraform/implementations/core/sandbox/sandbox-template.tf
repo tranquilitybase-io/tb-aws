cbeb32a31a5d923e80b259d72eb9ddfea371105a
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-18 09:38:09 -0600

    setting VPN configuration as variables

--- terraform/implementations/core/sandbox/sandbox-template.tf
+++ terraform/implementations/core/sandbox/sandbox-template.tf
@@ -1,36 +1,37 @@
 /*Using terraform VPC module, see https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/2.29.0*/
-module "vpc" {
+module "vpc-sandbox" {
   providers = {
     aws = aws.sandbox-account
   }
   source  = "terraform-aws-modules/vpc/aws"
   
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_sandbox_1_cidr
 
-  azs             = ["us-west-2a"]
-  private_subnets = ["10.0.101.0/24"]
-  public_subnets  = []
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_sandbox_1_private_subnets_cidr
+  public_subnets  =  var.vpc_sandbox_1_public_subnets_cidr
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
 
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "sandbox" }
 }
 
-module "vpc-2" {
+module "vpc-sandbox-2" {
   providers = {
     aws = aws.sandbox-account-2
   }
   source  = "terraform-aws-modules/vpc/aws"
   
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_sandbox_2_cidr
 
-  azs             = ["us-west-2a"]
-  private_subnets = ["10.0.101.0/24"]
-  public_subnets  = []
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_sandbox_1_private_subnets_cidr
+  public_subnets  = var.vpc_sandbox_1_public_subnets_cidr
+
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
 
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "sandbox" }
 }
