4a1e5585b94dba9008a5dd1de5ae2dd5c6d8d728
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-18 08:48:40 -0600

    TEst - VPC in both Sandbox accounts

--- terraform/implementations/core/sandbox/sandbox-template.tf
+++ terraform/implementations/core/sandbox/sandbox-template.tf
@@ -16,3 +16,21 @@ module "vpc" {
 
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "sandbox" }
 }
+
+module "vpc" {
+  providers = {
+    aws = aws.sandbox-account-2
+  }
+  source  = "terraform-aws-modules/vpc/aws"
+  
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a"]
+  private_subnets = ["10.0.101.0/24"]
+  public_subnets  = []
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id, (var.tag_key_name) = "sandbox" }
+}
