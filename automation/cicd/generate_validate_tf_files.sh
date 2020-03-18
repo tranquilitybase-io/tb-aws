bb0f08211fa90a6e6488f4fd2ab427047b740de8
Deiver Kiel <deiver.kiel@gft.com>
2020-03-18 00:40:58 -0600

    test 3 all working

--- automation/cicd/generate_validate_tf_files.sh
+++ automation/cicd/generate_validate_tf_files.sh
@@ -11,17 +11,18 @@ MAIN_PATH=$(pwd)
 AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform"
 
+cd ${TERRAFORM_PATH}
+
+sed -i "s;sharedservice_email;tl93372@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;logarchive_email;tl93373@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;security_email;test.gft.aws.03@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;network_email;test.gft.aws.04@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account_email;tb.test.logging.01@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account-2_email;tb.test.logging.02@gmail.com;" implementations/core/organization-variables.tf
+
 # Files preparation script
 python3 ${AUTOMATION_SCRIPTS}/terraform-pre-run.py
 
-cd ${TERRAFORM_PATH}
-
-#sed -i "s;sharedservice_email;tl93372@gmail.com;" implementations/core/organization-variables.tf
-#sed -i "s;logarchive_email;tl93373@gmail.com;" implementations/core/organization-variables.tf
-#sed -i "s;security_email;test.gft.aws.03@gmail.com;" implementations/core/organization-variables.tf
-#sed -i "s;network_email;test.gft.aws.04@gmail.com;" implementations/core/organization-variables.tf
-#sed -i "s;test-account_email;tb.test.logging.01@gmail.com;" implementations/core/organization-variables.tf
-#sed -i "s;test-account-2_email;tb.test.logging.02@gmail.com;" implementations/core/organization-variables.tf
 
 echo "-----------------------PRINTING MAIN.TF-------------------------------------------"
 cat main.tf 
