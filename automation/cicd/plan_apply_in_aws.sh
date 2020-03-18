1c99b912de61969321541db374ce940bfe254087
Deiver Kiel <deiver.kiel@gft.com>
2020-03-18 00:36:34 -0600

    test replace in plan and apply

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -11,7 +11,14 @@ AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform"
 
 cd ${TERRAFORM_PATH}
- 
+
+sed -i "s;sharedservice_email;tl93372@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;logarchive_email;tl93373@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;security_email;test.gft.aws.03@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;network_email;test.gft.aws.04@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account_email;tb.test.logging.01@gmail.com;" implementations/core/organization-variables.tf
+sed -i "s;test-account-2_email;tb.test.logging.02@gmail.com;" implementations/core/organization-variables.tf
+
 echo "------------------------TERRAFORM INIT--------------------------------------------"
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
