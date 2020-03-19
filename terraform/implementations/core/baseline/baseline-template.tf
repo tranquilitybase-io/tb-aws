5fd3497a7fef58a3122e1d32a9916370855793fd
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-19 11:40:33 -0600

    test remove sns topic

--- terraform/implementations/core/baseline/baseline-template.tf
+++ terraform/implementations/core/baseline/baseline-template.tf
@@ -2,6 +2,7 @@
 ### CONFIG SERVICE -->
 
 ### Topic for Config --->
+/*
 module "aws_lz_config_sns_topic" {
   source = "./modules/snstopic"
 
@@ -12,6 +13,7 @@ module "aws_lz_config_sns_topic" {
   sns_topic_name = var.config_topic_name
   required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
 }
+*/
 /*
 module "aws_lz_config_sns_topic-2" {
   source = "./modules/snstopic"
