c73cd3a34f448e07956c5c8a5f4d22ffd7b17259
cese <cese@gft.com>
2020-03-19 14:09:06 -0600

    Check Config-CloudTrail

--- terraform/modules/cloudtrail/main.tf
+++ terraform/modules/cloudtrail/main.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudtrail" "cloudtrail_default" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name                       = var.cloudtrail_name
   is_multi_region_trail      = var.multi_region_trail
   include_global_service_events = var.include_global_events
@@ -14,28 +16,38 @@ resource "aws_cloudtrail" "cloudtrail_default" {
 }
 
 resource "aws_sns_topic_policy" "sns_default_policy" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   arn = var.sns_topic_arn
   policy = data.aws_iam_policy_document.cloudtrail_alarm_policy.json
 }
 
 resource "aws_cloudwatch_log_group" "log_group_default" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name = "${var.cloudtrail_name}_default_log_group"
   retention_in_days = var.logs_retencion_days
   tags = var.required_tags
 }
 
 resource "aws_iam_role" "cloudtrail_role" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name               = "${var.cloudtrail_name}_role"
   assume_role_policy = data.aws_iam_policy_document.cloudtrail_assume_policy.json
   tags               = var.required_tags
 }
 
 resource "aws_iam_policy" "cloudtrail_access_policy" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name   = "${var.cloudtrail_name}_policy"
   policy = data.aws_iam_policy_document.cloudtrail_cloudwatch_policy.json
 }
 
 resource "aws_iam_policy_attachment" "cloudtrail_access_policy_attachment" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name       = "${var.cloudtrail_name}_policy_attachment"
   policy_arn = aws_iam_policy.cloudtrail_access_policy.arn
   roles      = [aws_iam_role.cloudtrail_role.name]
@@ -43,10 +55,14 @@ resource "aws_iam_policy_attachment" "cloudtrail_access_policy_attachment" {
 
 
 resource "aws_sns_topic" "cloudtrail" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   name = "${var.cloudtrail_name}_logging_sns_topic"
 }
 
 resource "aws_sns_topic_policy" "cloudtrail_topic_policy" {
+  count = length(var.cloudtrail_name) > 0 ? 1 : 0
+
   arn    = aws_sns_topic.cloudtrail.arn
   policy = data.aws_iam_policy_document.cloudtrail_sns.json
 }
--- terraform/modules/config/config-service/main.tf
+++ terraform/modules/config/config-service/main.tf
@@ -3,6 +3,8 @@
 #
 
 resource "aws_config_configuration_recorder" "main" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   name     = "${var.config_name}_recorder"
   role_arn = aws_iam_role.main.arn
 
@@ -13,6 +15,8 @@ resource "aws_config_configuration_recorder" "main" {
 }
 
 resource "aws_config_delivery_channel" "main" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   name           = aws_config_configuration_recorder.main.name
   s3_bucket_name = var.config_logs_bucket
   s3_key_prefix  = var.s3_log_prefix
@@ -25,23 +29,31 @@ resource "aws_config_delivery_channel" "main" {
 }
 
 resource "aws_config_configuration_recorder_status" "main" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   name       = aws_config_delivery_channel.main.name
   is_enabled = true
 }
 
 resource "aws_iam_role" "main" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   name               = "${var.config_name}_iam_role"
   assume_role_policy = data.aws_iam_policy_document.aws_config_role_policy.json
   tags = var.config_tags
 }
 
 resource "aws_iam_policy_attachment" "managed_policy" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   name       = "${var.config_name}_managed_policy"
   roles      = [aws_iam_role.main.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
 resource "aws_sns_topic_policy" "sns_config_default_policy" {
+  count = length(var.config_name) > 0 ? 1 : 0
+
   arn = var.sns_topic_arn  
   policy = data.aws_iam_policy_document.config_sns.json
 }
