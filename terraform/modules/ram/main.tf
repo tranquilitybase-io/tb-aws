d5e6f1dfc2f4cfd81e0b66c12d01b5cefdcb9e77
iota <ignacio.tissera@gft.com>
2020-03-19 16:44:11 -0600

    Adding organizations as principal - Fixing using ID instead of ARN - RAM Share

--- terraform/modules/ram/main.tf
+++ terraform/modules/ram/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_ram_resource_share" "aws_lz_ram_shared" {
-  count = var.ram_create && var.ram_share ? 1 : 0
+  count = length(var.ram_name)  ? 1 : 0
 
   name   = var.ram_name
   allow_external_principals = var.ram_allow_external_principals
@@ -8,14 +8,14 @@ resource "aws_ram_resource_share" "aws_lz_ram_shared" {
 }
 
 resource "aws_ram_resource_association" "aws_lz_ram_association" {
-  count = var.ram_create && var.ram_share ? 1 : 0
+  count = length(var.ram_resource_arn) ? 1 : 0
 
   resource_arn       = var.ram_resource_arn
   resource_share_arn = aws_ram_resource_share.aws_lz_ram_shared[0].id
 }
 
 resource "aws_ram_principal_association" "aws_lz_ram_principal_association" {
-  count = var.ram_create && var.ram_share ? length(var.ram_principals) : 0
+  count = length(var.ram_principals) ? length(var.ram_principals) : 0
 
   principal = var.ram_principals
   resource_share_arn = aws_ram_resource_share.aws_lz_ram_shared[0].arn
