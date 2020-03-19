c322dbf9498f2d96088efd82cb41f239defdc2b7
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-19 15:15:32 -0600

    SNS topic on both sandbox account

--- terraform/implementations/core/baseline/baseline-template.tf
+++ terraform/implementations/core/baseline/baseline-template.tf
@@ -14,7 +14,6 @@ module "aws_lz_config_sns_topic" {
   required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
 }
 
-/*
 module "aws_lz_config_sns_topic-2" {
   source = "./modules/snstopic"
 
@@ -26,7 +25,7 @@ module "aws_lz_config_sns_topic-2" {
   required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.sandbox_account_id, (var.tag_key_name) = "config" }
 }
 ### <---
-*/
+
 /*
 
 module "aws_lz_config_service" {
