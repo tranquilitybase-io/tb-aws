27ebc8668151214038f688a1aa8d4d2f7ad6e2ab
iota <ignacio.tissera@gft.com>
2020-03-19 15:25:43 -0600

    Removed commented line

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -40,7 +40,7 @@ module "aws_lz_egress_vpc" {
 
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.network_account_id, (var.tag_key_name) = "network" }
 }
-*/
+
 
 module "aws_lz_aws_ram_share_tg" {
   source = "./modules/ram"
