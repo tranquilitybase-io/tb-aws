d7d4043923c333374be43247fb5e86c06a0a79c0
iota <ignacio.tissera@gft.com>
2020-03-19 15:45:12 -0600

    Adding organizations as principal - Fixing

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -47,7 +47,7 @@ module "aws_lz_aws_ram_share_tg" {
 
   ram_name = "AWS_LZ_TG"
   ram_resource_arn = module.aws_lz_tgw.tgw_arn
-  ram_principals = module.aws_lz_organization_main.org_arn
+  ram_principals = module.aws_lz_organization_main.roots[0].arn
   ram_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "aws-ram-tg" }
 
   providers = {
