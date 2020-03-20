f29c95163994909327bc545631a9f66eec867ac5
iota <ignacio.tissera@gft.com>
2020-03-20 15:36:09 -0600

    Print result from enable ram sharing

--- terraform/modules/extensions/ram/aws_ram.py
+++ terraform/modules/extensions/ram/aws_ram.py
@@ -5,6 +5,7 @@ from boto3.session import Session
 def enable_aws_ram_organization():
     client = boto3.client('ram')
     response = client.enable_sharing_with_aws_organization()
+    print(response)
 
 if __name__== "__main__":
     enable_aws_ram_organization()
