a5c114e53bfaa7e12c920fac5fbe6f5759d2f9ff
iota <ignacio.tissera@gft.com>
2020-03-20 16:25:35 -0600

    Prevent destroy null ref module

--- terraform/implementations/core/organization-template.tf
+++ terraform/implementations/core/organization-template.tf
@@ -96,7 +96,7 @@ module "aws_lz_account_network" {
   account_role_name = var.org_admin_role
   org_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_organization_main.master_account_id, (var.tag_key_name) = "organization" }
   account_parent_id = module.aws_lz_ou_core.ou_id
-}
+}   
 
 
 module "enable_aws_ram_for_organization" {
