02f62aea953e4ebcb13d8d002eb368342bf1ac01
iota <ignacio.tissera@gft.com>
2020-03-20 12:49:18 -0600

    Adding null-reference resource in order to enable AWS RAM in organization | Changing python for python3 | Re adding sharing | Create dependencies on resource for warranty permission execution first | Splitting module in two

--- terraform/modules/ram/main.tf
+++ terraform/modules/ram/main.tf
@@ -5,6 +5,7 @@ resource "aws_ram_resource_share" "aws_lz_ram_shared" {
   allow_external_principals = var.ram_allow_external_principals
   tags = var.ram_tags
 
+  depends_on = [null_resource.enable_aws_ram_organization]
 }
 
 resource "aws_ram_resource_association" "aws_lz_ram_association" {
@@ -12,6 +13,8 @@ resource "aws_ram_resource_association" "aws_lz_ram_association" {
 
   resource_arn       = var.ram_resource_arn
   resource_share_arn = aws_ram_resource_share.aws_lz_ram_shared[0].id
+
+  depends_on = [aws_ram_resource_share.aws_lz_ram_shared]
 }
 
 resource "aws_ram_principal_association" "aws_lz_ram_principal_association" {
@@ -19,6 +22,8 @@ resource "aws_ram_principal_association" "aws_lz_ram_principal_association" {
 
   principal = var.ram_principals
   resource_share_arn = aws_ram_resource_share.aws_lz_ram_shared[0].arn
+
+  depends_on = [aws_ram_resource_association.aws_lz_ram_association]
 }
 
 
