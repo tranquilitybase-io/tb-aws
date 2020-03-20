f4fcafe999b8ad47aa8b8e286da78fefaa2fe9e9
iota <ignacio.tissera@gft.com>
2020-03-19 19:27:15 -0600

    Adding organizations as principal - Fixing using ID instead of ARN - Removing module from TFstate

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -16,11 +16,13 @@ echo "------------------------TERRAFORM INIT------------------------------------
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
-terraform apply -auto-approve
+#terraform apply -auto-approve
 
 
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
+
+terraform state rm module.aws_lz_ram_principal_association
 
 #terraform state rm module.vpc-network-account
 #terraform state rm module.aws_lz_config_service-2
