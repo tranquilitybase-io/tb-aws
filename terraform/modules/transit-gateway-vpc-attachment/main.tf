ee028ae45102eb1459418b6c47148fb43f9c7f96
cese <cese@gft.com>
2020-03-23 14:24:49 -0600

    Fixed attach module - Attach Egress VPC with TGW

--- terraform/modules/transit-gateway-vpc-attachment/main.tf
+++ terraform/modules/transit-gateway-vpc-attachment/main.tf
@@ -1,4 +1,5 @@
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-vpc-attachment" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attachment" {
+  count = length(var.account_id) > 0 ? 1 : 0
   subnet_ids         = var.subnets_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
