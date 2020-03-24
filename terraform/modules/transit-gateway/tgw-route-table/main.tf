880e94dff569ce779f9d436c64943f029f37194f
csanchez <cesar.sanchez@gft.com>
2020-03-24 10:50:07 -0600

    TGW route created - fixed count

--- terraform/modules/transit-gateway/tgw-route-table/main.tf
+++ terraform/modules/transit-gateway/tgw-route-table/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_ec2_transit_gateway_route" "aws_lz_tgw_route" {
-  count = length(var.attach_id) > 0 ? 1 : 0
+  count = length(var.tgw_id) > 0 ? 1 : 0
 
   destination_cidr_block         = "0.0.0.0/0"
   transit_gateway_attachment_id  = var.attach_id
