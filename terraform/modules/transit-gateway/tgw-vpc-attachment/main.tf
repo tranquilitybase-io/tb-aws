0d00e59a7bdb7571d229d35e8f43b6ed84ea352a
csanchez <cesar.sanchez@gft.com>
2020-03-24 11:01:08 -0600

    TGW route created - fixed attachment name

--- terraform/modules/transit-gateway/tgw-vpc-attachment/main.tf
+++ terraform/modules/transit-gateway/tgw-vpc-attachment/main.tf
@@ -1,11 +1,12 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attachment" {
-  #count = length(var.account_id) > 0 ? 1 : 0
+  count = length(var.transit_gateway_id) > 0 ? 1 : 0
   subnet_ids         = var.subnets_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
+
   tags = merge(
   {
-    "Name" = format("AWSLZ_TWG_VPC_%s", var.account_id)
+    "Name" = format("aws_lz_vpc_%s", var.account_id)
   },
   var.tags
   )
