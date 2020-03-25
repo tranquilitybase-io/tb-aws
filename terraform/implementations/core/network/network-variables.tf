1f58cca39801ea704abede09004116a365ff2f25
csanchez <cesar.sanchez@gft.com>
2020-03-25 07:55:02 -0600

    Create Ingress VPC

--- terraform/implementations/core/network/network-variables.tf
+++ terraform/implementations/core/network/network-variables.tf
@@ -1,4 +1,14 @@
 
+variable "ingress_vpc_name" {
+  default = "aws_lz_ingress_vpc"
+  description = "Ingress VPC Name"
+}
+
+variable "ingress_vpc_cidr"{
+  description = "Egress VPC CIDR"
+  default = "10.98.0.0/22"
+}
+
 variable "egress_vpc_name" {
   default = "aws_lz_egress_vpc"
   description = "Egress VPC Name"
