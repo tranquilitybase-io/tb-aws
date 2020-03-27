ffd43cbd755331b16d2317426fad4805fceefa03
Deiver Kiel <deiver.kiel@gft.com>
2020-03-27 09:59:27 -0600

    Remove ec2 folder

--- terraform/modules/ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "instances" {
-  ami           = var.ami_version
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-
-  tags = {
-    Name = "Ubuntu"
-  }
-}
