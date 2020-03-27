5891758736e85ae46a5709ed5042e9de3ac588ff
Deiver Kiel <deiver.kiel@gft.com>
2020-03-27 07:18:40 -0600

    fix in installation

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -1,5 +1,5 @@
 locals {
-  nginx_install = "codename=$(lsb_release -c | awk '{print $2}'); echo \"deb http://nginx.org/packages/mainline/ubuntu/ bionic nginx \" >> /etc/apt/sources.list; wget http://nginx.org/keys/nginx_signing.key; apt-key add nginx_signing.key -y; apt-get update -y; apt install nginx -y; systemctl start nginx"
+  nginx_install = "echo deb http://nginx.org/packages/mainline/ubuntu/ bionic nginx >> /etc/apt/sources.list; wget http://nginx.org/keys/nginx_signing.key; apt-key add nginx_signing.key; apt-get update -y; apt install nginx -y; systemctl start nginx"
 }
 
 #Create TGW
