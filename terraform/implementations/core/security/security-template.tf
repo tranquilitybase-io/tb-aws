2dd733bcea599793b01a03853f1be2d4bc722e2e
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-31 11:42:38 -0600

    test security admin role

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -11,7 +11,7 @@ module "aws_lz_aggregate_security_sns_topic" {
 
 ############################ SECURITY ADMIN ROLES #####################################
 
-data "aws_iam_policy_document" "aws_lz_assume_role_network" {
+data "aws_iam_policy_document" "aws_lz_assume_role_security" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -28,6 +28,6 @@ data "aws_iam_policy_document" "aws_lz_assume_role_network" {
 module "aws_lz_iam_security_admin_master" {
   source = "./modules/iam"
   role_name = "${var.security_role_name}_${local.current_account_id}"
-  assume_role_policy = data.aws_iam_policy_document.aws_lz_role_policy_security
+  assume_role_policy = "${data.aws_iam_policy_document.aws_lz_assume_role_security.json}"
   role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
 }
