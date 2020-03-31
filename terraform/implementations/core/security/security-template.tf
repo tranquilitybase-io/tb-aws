1210a00833272d68538ce8daf00c738f28763d94
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-31 11:26:17 -0600

    add security admin role to master account

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -9,3 +9,25 @@ module "aws_lz_aggregate_security_sns_topic" {
   required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_account_security.account_id, (var.tag_key_name) = "Aggregate Security" }
 }
 
+############################ SECURITY ADMIN ROLES #####################################
+
+data "aws_iam_policy_document" "aws_lz_assume_role_network" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.security_account_id}:root"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+
+module "aws_lz_iam_security_admin_master" {
+  source = "./modules/iam"
+  role_name = "${var.security_role_name}_${local.current_account_id}"
+  assume_role_policy = data.aws_iam_policy_document.aws_lz_role_policy_security.json
+  role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
+}
--- terraform/implementations/core/security/security-variables.tf
+++ terraform/implementations/core/security/security-variables.tf
@@ -3,3 +3,10 @@ variable "aggregate_topic_name" {
   default = "aws_lz_aggregate_security_sns_topic"
   description = "Name of topic for Security Account"
 }
+
+
+############
+variable "security_role_name"{
+  default = "SecurityAdminRole"
+  description = "Name of the Security admin role to be assumed"
+}
