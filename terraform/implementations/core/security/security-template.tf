b1f2bf277bb30c5df7dfb5c243c4cb4725017d93
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-03-31 12:50:42 -0600

    commenting security roles

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -25,26 +25,26 @@ data "aws_iam_policy_document" "aws_lz_assume_role_security" {
 }
 
 
-module "aws_lz_iam_security_admin_master" {
-  source = "./modules/iam"
-  role_name = "${var.security_role_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.aws_lz_assume_role_security.json}"
-  role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
+# module "aws_lz_iam_security_admin_master" {
+#   source = "./modules/iam"
+#   role_name = "${var.security_role_name}"
+#   assume_role_policy = "${data.aws_iam_policy_document.aws_lz_assume_role_security.json}"
+#   role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
 
-  #attachment
-  policy_attach_name = "${var.policy_attach_security_admin}"
-  policy_attach_roles = ["${var.security_role_name}"]
-  policy_arn = var.administrator_access_arn
-}
+#   #attachment
+#   policy_attach_name = "${var.policy_attach_security_admin}"
+#   policy_attach_roles = ["${var.security_role_name}"]
+#   policy_arn = var.administrator_access_arn
+# }
 
-module "aws_lz_iam_security_audit_master" {
-  source = "./modules/iam"
-  role_name = "${var.security_role_name_audit}"
-  assume_role_policy = "${data.aws_iam_policy_document.aws_lz_assume_role_security.json}"
-  role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
+# module "aws_lz_iam_security_audit_master" {
+#   source = "./modules/iam"
+#   role_name = "${var.security_role_name_audit}"
+#   assume_role_policy = "${data.aws_iam_policy_document.aws_lz_assume_role_security.json}"
+#   role_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.current_account_id }
 
-  #attachment
-  policy_attach_name = "${var.policy_attach_security_audit}"
-  policy_attach_roles = ["${var.security_role_name_audit}"]
-  policy_arn = var.read_only_access_arn
-}
+#   #attachment
+#   policy_attach_name = "${var.policy_attach_security_audit}"
+#   policy_attach_roles = ["${var.security_role_name_audit}"]
+#   policy_arn = var.read_only_access_arn
+# }
