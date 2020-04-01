4ef41296962e6cd30fe4d49131a649dc6ff00afa
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-04-01 10:44:10 -0600

    policy attachment with role name as depndency

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -32,6 +32,7 @@ resource "aws_iam_policy_attachment" "aws_lz_iam_attach_policy" {
 
 resource "aws_iam_role_policy_attachment" "role_policy_attach" {
   count      = var.role_policy_attach == true ? 1 : 0
-  role       = var.role_name
+
+  role       = aws_iam_role.aws_lz_iam_role.name
   policy_arn = var.policy_arn
 }
