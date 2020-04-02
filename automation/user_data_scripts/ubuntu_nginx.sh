24ac77ebfdb3b026c7bcb709d26e24dfec6d400d
Deiver Kiel <deiver.kiel@gft.com>
2020-04-02 00:14:06 -0600

    Send web requests

--- automation/user_data_scripts/ubuntu_nginx.sh
+++ automation/user_data_scripts/ubuntu_nginx.sh
@@ -6,6 +6,21 @@ echo "deb http://nginx.org/packages/mainline/ubuntu/ ${codename} nginx" >> /etc/
 wget http://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
 
-apt-get update -y
+apt update -y
 apt install nginx -y
+
+cd /etc/nginx/conf.d
+cat <<EOF>> app1.conf
+server {
+  listen 80;
+  listen [::]:80;
+
+  server_name EIP;
+
+  location / {
+      proxy_pass http://internal_server_ip:80/;
+  }
+}
+EOF
+
 systemctl start nginx
