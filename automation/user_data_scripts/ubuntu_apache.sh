12930673c18882d3085682858a47a419d9993c45
Deiver Kiel <deiver.kiel@gft.com>
2020-04-02 01:00:49 -0600

    path to files

---
+++ automation/user_data_scripts/ubuntu_apache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo "deb http://nginx.org/packages/mainline/ubuntu/ ${codename} nginx" >> /etc/apt/sources.list
+#wget http://nginx.org/keys/nginx_signing.key
+#apt-key add nginx_signing.key
+apt update -y
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2 
