10c4798255239a85ae18161c09d0d60e01b4db68
Deiver Kiel <deiver.kiel@gft.com>
2020-04-02 00:05:22 -0600

    codename

--- automation/user_data_scripts/ubuntu_nginx.sh
+++ automation/user_data_scripts/ubuntu_nginx.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+codename=$(lsb_release -c | awk '{print $2}')
 echo "deb http://nginx.org/packages/mainline/ubuntu/ ${codename} nginx" >> /etc/apt/sources.list
+
 wget http://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
+
 apt-get update -y
 apt install nginx -y
 systemctl start nginx
