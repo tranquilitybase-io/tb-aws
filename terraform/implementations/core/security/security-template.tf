ebbba6aecaf3a25ecad4e43447a09a41130fd3a6
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-04-03 11:00:12 -0600

    add policy to SNS

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -18,4 +18,40 @@ module "aws_lz_guardduty_sns_topic" {
 
   sns_topic_name = var.guardduty_topic_name
   required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.seccurity_account_id, (var.tag_key_name) = "Guardduty findings" }
+
+  attach_policy = true
+  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+}
+
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Publish"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "${local.seccurity_account_id}",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    resources = [
+      "${module.aws_lz_aggregate_security_sns_topic.topic_arn}",
+    ]
+
+    sid = "__default_statement_ID"
+  }
 }
--- terraform/modules/snstopic/main.tf
+++ terraform/modules/snstopic/main.tf
@@ -11,3 +11,9 @@ resource "aws_sns_topic" "sns_topic_default" {
   kms_master_key_id = var.kms_master_key_id
   tags   = var.required_tags
 }
+
+resource "aws_sns_topic_policy" "default_access_policy" {
+  count = var.attach_policy == true 1 : 0
+  arn = aws_sns_topic.sns_topic_default[count.index].arn
+  policy = var.policy
+}
