4f853726485d970e482c26b77441ba4a4a7e6c99
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-04-03 11:27:42 -0600

    fix typo

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -17,7 +17,7 @@ module "aws_lz_guardduty_sns_topic" {
   }
 
   sns_topic_name = var.guardduty_topic_name
-  required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.seccurity_account_id, (var.tag_key_name) = "Guardduty findings" }
+  required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.security_account_id, (var.tag_key_name) = "Guardduty findings" }
 
   attach_policy = true
   policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
