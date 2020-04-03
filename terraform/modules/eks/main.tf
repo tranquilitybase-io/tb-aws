736b7685a48678b26ebe7c18979c4fdd2ad72c9c
iota <ignacio.tissera@gft.com>
2020-04-03 17:37:19 -0600

    First version of EKS cluster role and policies

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,4 +2,44 @@
 * # GFT AWS LZ EKS Service Terraform module
 *
 * Usage: Terraform module which creates k8s resources using AWS EKS.
+*   - Each cluster will have a user with its attached policies
 */
+
+resource "aws_iam_role" "aws_lz_gft_eks_role" {
+  count = length(var.eks_role_name) > 0 ? 1 : 0
+
+  name = var.eks_role_name
+  assume_role_policy = var.eks_user_policy
+}
+
+resource "aws_eks_cluster" "aws_lz_gft_k8s_cluster" {
+  count = length(var.eks_name) > 0 ? 1 : 0
+  name     = var.eks_name
+  role_arn = aws_iam_role.aws_lz_gft_eks_role[count.index].arn
+
+  vpc_config {
+    subnet_ids = var.eks_vpc_subnet_list
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.aws_lz_gft_AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.aws_lz_gft_AmazonEKSServicePolicy",
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lz_gft_AmazonEKSClusterPolicy" {
+  count = length(var.eks_role_name) > 0 ? 1 : 0
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.aws_lz_gft_eks_role[count.index].arn
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lz_gft_AmazonEKSServicePolicy" {
+  count = length(var.eks_role_name) > 0 ? 1 : 0
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.aws_lz_gft_eks_role[count.index].arn
+}
+
+
+
--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,1 +1,38 @@
+variable "eks_name" {
+  description = "EKS name"
+  type = string
+}
+
+variable "eks_role_arn" {
+  description = "EKS role ARN"
+  type = string
+}
+
+variable "eks_vpc_subnet_list" {
+  description = "List of subnets in the VPC to be used by EKS"
+  type = list(string)
+}
+
+variable "eks_role_name" {
+  type = string
+  description = "EKS role name"
+}
+variable "eks_user_policy" {
+  type = string
+  description = "Defaul policy applied to EKS user"
+  default = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
 
