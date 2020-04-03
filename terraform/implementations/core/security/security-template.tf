ddf1b2c56fc9f5549702b625187d08ce8456078e
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-04-03 08:54:36 -0600

    add SNS topic for guardduty findings

--- terraform/implementations/core/security/security-template.tf
+++ terraform/implementations/core/security/security-template.tf
@@ -8,3 +8,14 @@ module "aws_lz_aggregate_security_sns_topic" {
   sns_topic_name = var.aggregate_topic_name
   required_tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = module.aws_lz_account_security.account_id, (var.tag_key_name) = "Aggregate Security" }
 }
+
+module "aws_lz_guardduty_sns_topic" {
+  source = "./modules/snstopic"
+
+  providers = {
+    aws = aws.security-account
+  }
+
+  sns_topic_name = var.guardduty_topic_name
+  required_tags  = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.seccurity_account_id, (var.tag_key_name) = "Guardduty findings" }
+}
