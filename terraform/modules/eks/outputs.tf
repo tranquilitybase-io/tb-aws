aea2ddce322514226b2aa6340a5ae931dc771e5a
Deiver Kiel <deiver.kiel@gft.com>
2020-04-05 22:36:20 -0600

    remove outputs

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,12 +1,12 @@
-output "unique_id" {
-  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.id, [""])[0]
-  description = "ID of the role"
-}
-output "role_arn" {
-  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.arn, [""])[0]
-  description = "Role ARN"
-}
-output "role_name" {
-  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.name, [""])[0]
-  description = "Role Name"
-}
+#output "unique_id" {
+#  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.id, [""])[0]
+#  description = "ID of the role"
+#}
+#output "role_arn" {
+#  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.arn, [""])[0]
+#  description = "Role ARN"
+#}
+#output "role_name" {
+#  value = concat(aws_iam_role.aws_lz_gft_eks_role.*.name, [""])[0]
+#  description = "Role Name"
+#}
