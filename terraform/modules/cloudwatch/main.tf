0b930a253f220d7784f627c06bde63333af6c410
ronald.hernandez@gft.com <ronald.hernandez@gft.com>
2020-04-06 09:19:06 -0600

    add missing }

--- terraform/modules/cloudwatch/main.tf
+++ terraform/modules/cloudwatch/main.tf
@@ -9,6 +9,7 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
   description = var.event_description
 
   event_pattern = var.event_pattern
+}
 
 resource "aws_cloudwatch_event_target" "event_targer" {
   rule      = var.event_name
