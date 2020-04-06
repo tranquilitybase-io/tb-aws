30080b62406abcd1cf831683b129f369097fbfa9
Deiver Kiel <deiver.kiel@gft.com>
2020-04-05 22:39:53 -0600

    remove interpolations

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -19,10 +19,10 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "aws_lz_gft_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.aws_lz_gft_eks_iam_role.name}"
+  role       = aws_iam_role.aws_lz_gft_eks_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "aws_lz_gft_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.aws_lz_gft_eks_iam_role.name}"
+  role       = aws_iam_role.aws_lz_gft_eks_iam_role.name
 }
