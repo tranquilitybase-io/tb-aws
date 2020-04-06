ce4e889fb50db6bc80e3865422f12bfc8456d9a1
Deiver Kiel <deiver.kiel@gft.com>
2020-04-05 23:17:42 -0600

    validate role_arn

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -13,3 +13,17 @@ resource "aws_iam_role_policy_attachment" "aws_lz_gft_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.aws_lz_gft_eks_iam_role.name
 }
+
+resource "aws_eks_cluster" "eks_ingress_cluster" {
+  name     = "eks_ingress_cluster"
+  role_arn = aws_iam_role.aws_lz_gft_.arn
+
+  vpc_config {
+    subnet_ids = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.aws_lz_gft_AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.aws_lz_gft_AmazonEKSServicePolicy",
+  ]
+}
