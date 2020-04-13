1f5161d474ec18c4eeafdc58d662767e766235ed
Deiver Kiel <deiver.kiel@gft.com>
2020-04-13 13:29:29 -0600

    [skip CI]

---
+++ terraform/modules/key-pairs/variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "Key name to use in AWS"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Key pair generate in the deployment process"
+  type        = string
+}
