fd2631b9b1b5a5ffa6dedfae297a9a7f7afc1e9a
Deiver Kiel <deiver.kiel@gft.com>
2020-04-14 15:32:38 -0600

    Remove current key

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -367,6 +367,7 @@ module "ingress_eks_cluster" {
 }
 # END Create EKS cluster
 
+/*
 # Key pair
 module "network_account_keypair" {
   source = "./modules/key-pairs"
@@ -379,7 +380,7 @@ module "network_account_keypair" {
   tags        = { generation_date = var.env_generation_date }
 }
 # END Key pair
-
+*/
 
 ### </ In-line VPC
 module "aws_lz_inline_vpc" {
