c63346ba5e3deb2cfe9185200a9870ac82dd37a5
Deiver Kiel <deiver.kiel@gft.com>
2020-04-14 22:45:05 -0600

    test run again

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -359,7 +359,7 @@ module "network_account_keypair" {
 
   key_name    = var.deployment_key_name
   public_key  = var.env_deployment_key
-  tags        = { generation_date = var.env_generation_date }
+  tags        = { generation_date = var.env_generation_date } 
 }
 # END Key pair
 
