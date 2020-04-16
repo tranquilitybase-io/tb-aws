605272f16ceb1ba7547058d1a6c02f3dd5bb7dd6
Deiver Kiel <deiver.kiel@gft.com>
2020-04-16 10:55:50 -0600

    Deploy reverse proxy

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -461,7 +461,7 @@ module "aws_lz_tgw_inline_vpc_route"{
 }
 ### END In-line VPC />
 
-/*
+
 #EC2 Instances
 # NGINX Reverse proxy
 module "network_reverse_proxy_ec2_instance" {
@@ -480,7 +480,7 @@ module "network_reverse_proxy_ec2_instance" {
   private_ip = var.network_reverse_proxy_private_ip
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.network_account_id, (var.tag_key_name) = "network" }
 }
-*/ 
+
 
 # Bastion
 module "ec2_instance_bastion" {
