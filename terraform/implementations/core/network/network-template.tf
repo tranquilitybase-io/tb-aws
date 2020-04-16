4b78b08ce56183458ba5263890eca9060fab62d1
csanchez <cesar.sanchez@gft.com>
2020-04-15 21:00:32 -0600

    NetMon Instance - user data

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -516,7 +516,7 @@ module "aws_lz_net_monitor_instance" {
   instance_type = var.t2_micro_instance_type
   subnet_id = element(module.aws_lz_inline_vpc.private_subnets,0)
   vpc_security_group_ids = [module.netmon_security_group.this_security_group_id]
-  user_data = replace(user_data_raw,"NETMON_EMAIL",var.email_netmon)
+  user_data = replace(local.user_data_raw,"NETMON_EMAIL",var.email_netmon)
   key_name = module.network_account_keypair.key_name #var.network_account_key_name
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.network_account_id, (var.tag_key_name) = "network" }
 }
