9861e7dbefc09330212dbcc002c11aaac862ac0d
csanchez <cesar.sanchez@gft.com>
2020-04-20 15:16:51 -0600

    Testing Tags

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -8,12 +8,9 @@ locals {
   gn_tags = merge(  {"Name" = format("%s", var.global_network_name)}, var.tags,)
 }
 
-#--region ${var.region}
-#--description ${var.global_network_description}
-
 resource "null_resource" "aws_lz_ena_net_manager" {
   provisioner "local-exec" {
-    command = "aws networkmanager create-global-network --description '${var.global_network_description}'"
+    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags {merge(  {'Name' = format('%s', var.global_network_name)}, var.tags,)}"
     #command = "aws networkmanager create-global-network --description 'Head offices global network' --region us-west-2 --profile network-account"
   }
 }
