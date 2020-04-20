e66a2dc44ac5048bca467bd0efdaf86fc3163044
csanchez <cesar.sanchez@gft.com>
2020-04-20 16:46:09 -0600

    Network Manager - Check Tags tolist()

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -5,7 +5,7 @@
 */
 
 locals {
-  gn_tags = tolist(merge(  {"Name" = format("%s", var.global_network_name)}, var.tags))
+  gn_tags = merge(map({'Name' = format('%s', var.global_network_name)}, var.tags))
 }
 
 resource "null_resource" "aws_lz_ena_net_manager" {
