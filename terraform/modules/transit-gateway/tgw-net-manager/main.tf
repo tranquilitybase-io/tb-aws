2bd8bb0c325a38ef7fe73fac69aa4cc7612fede7
csanchez <cesar.sanchez@gft.com>
2020-04-20 16:23:28 -0600

    Network Manager - Check Tags

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -10,7 +10,7 @@ locals {
 
 resource "null_resource" "aws_lz_ena_net_manager" {
   provisioner "local-exec" {
-    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags {merge(  {'Name' = format('%s', var.global_network_name)}, var.tags)}"
+    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags ${local.gn_tags}"
     #command = "aws networkmanager create-global-network --description 'Head offices global network' --region us-west-2 --profile network-account"
   }
 }
