daeddf852933f407ae3b99b4565e5329dea9fea6
csanchez <cesar.sanchez@gft.com>
2020-04-21 16:25:18 -0600

    Creating Global Network - Registering TGW

--- terraform/modules/transit-gateway/tgw-net-manager/data.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/data.tf
@@ -4,5 +4,5 @@ data "template_file" "log_name" {
 
 data "local_file" "create_globalnet" {
   filename = data.template_file.log_name.rendered
-  depends_on = [null_resource.aws_lz_ena_net_manager]
+  depends_on = [null_resource.aws_lz_global_network]
 }
