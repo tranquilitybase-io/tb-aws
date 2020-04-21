0bf28487a8abe2a4f3fd886e13e6329634ac3939
csanchez <cesar.sanchez@gft.com>
2020-04-21 08:51:48 -0600

    Global Net output

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -11,6 +11,7 @@ locals {
 resource "null_resource" "aws_lz_ena_net_manager" {
   provisioner "local-exec" {
     command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags Key=Name,Value=${var.global_network_name}"
-    #command = "aws networkmanager create-global-network --description 'Head offices global network' --region us-west-2 --profile network-account"
   }
 }
+
+
---
+++ terraform/modules/transit-gateway/tgw-net-manager/output.tf
@@ -0,0 +1,12 @@
+data "template_file" "log_name" {
+  template = "./modules/transit-gateway/tgw-net-manager/output.log"
+}
+
+data "local_file" "create-globalnet" {
+  filename = data.template_file.log_name.rendered
+  depends_on = ["null_resource.aws_lz_ena_net_manager"]
+}
+
+output "aws-cli-output" {
+  value = data.local_file.create-globalnet.content
+}
