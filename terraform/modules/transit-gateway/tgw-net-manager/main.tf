68101aaa484131da65474d0f507620d465b36713
csanchez <cesar.sanchez@gft.com>
2020-04-21 14:53:59 -0600

    GN commented

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -10,16 +10,15 @@ locals {
 
 resource "null_resource" "aws_lz_ena_net_manager" {
     provisioner "local-exec" {
-    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags Key=Name,Value=${var.global_network_name} > ${data.template_file.log_name.rendered}"
+    command = "aws networkmanager create-global-network --description '${local.file}' --tags Key=Name,Value=${var.global_network_name} > ${data.template_file.log_name.rendered}"
   }
 }
 
 resource "null_resource" "aws_lz_register_tgw_globalnet" {
   provisioner "local-exec" {
-    command = "aws networkmanager register-transit-gateway --global-network-id ${local.file.GlobalNetworkId} --transit-gateway-arn ${var.tgw_arn}"
+    command = "aws networkmanager register-transit-gateway --global-network-id ${local.file.GlobalNetwork.GlobalNetworkId} --transit-gateway-arn ${var.tgw_arn}"
   }
   depends_on = [null_resource.aws_lz_ena_net_manager]
-
 }
 
 
