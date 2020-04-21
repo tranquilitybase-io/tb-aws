59d2b9a8e84bdb09390c03bd6d8bef8513b62c6f
csanchez <cesar.sanchez@gft.com>
2020-04-21 16:21:37 -0600

    Creating Global Network - Registering TGW

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -545,8 +545,6 @@ module "aws_lz_net_monitor_instance" {
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.network_account_id, (var.tag_key_name) = "network" }
 }
 
-/*
-
 ###BEGIN Network Manager
 ###Create Global Network
 module "aws_lz_create_global_network" {
@@ -563,4 +561,3 @@ module "aws_lz_register_tgw_globalnet" {
   tgw_arn = module.aws_lz_tgw.tgw_arn
 }
 ###END Network Manager
-*/
