02a2ec356c131e294d37ce7570fd82942a56d077
csanchez <cesar.sanchez@gft.com>
2020-04-21 16:48:18 -0600

    GN commented

--- terraform/implementations/core/network/network-template.tf
+++ terraform/implementations/core/network/network-template.tf
@@ -544,6 +544,7 @@ module "aws_lz_net_monitor_instance" {
   disable_api_termination = true
   tags = { (var.tag_key_project_id) = var.awslz_proj_id, (var.tag_key_environment) = var.awslz_environment, (var.tag_key_account_id) = local.network_account_id, (var.tag_key_name) = "network" }
 }
+/*
 
 ###BEGIN Network Manager
 ###Create Global Network
@@ -561,3 +562,5 @@ module "aws_lz_register_tgw_globalnet" {
   tgw_arn = module.aws_lz_tgw.tgw_arn
 }
 ###END Network Manager
+*/
+
