41f97d6a1c9e627572a1432a65b3ece596265807
csanchez <cesar.sanchez@gft.com>
2020-04-21 09:06:33 -0600

    Global Net output

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -8,6 +8,15 @@ locals {
   gn_tags = merge({"Name" = format("%s", var.global_network_name)}, var.tags)
 }
 
+data "template_file" "log_name" {
+  template = "./modules/transit-gateway/tgw-net-manager/output.log"
+}
+
+data "local_file" "create-globalnet" {
+  filename = data.template_file.log_name.rendered
+  depends_on = [null_resource.aws_lz_ena_net_manager]
+}
+
 resource "null_resource" "aws_lz_ena_net_manager" {
   provisioner "local-exec" {
     command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags Key=Name,Value=${var.global_network_name} > ${data.template_file.log_name.rendered}"
