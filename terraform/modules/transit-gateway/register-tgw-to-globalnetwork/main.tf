22b6789571899181145f487edbbc4ce48e3058bf
csanchez <cesar.sanchez@gft.com>
2020-04-21 16:28:07 -0600

    Creating Global Network - Registering TGW

--- terraform/modules/transit-gateway/register-tgw-to-globalnetwork/main.tf
+++ terraform/modules/transit-gateway/register-tgw-to-globalnetwork/main.tf
@@ -6,7 +6,7 @@
 
 resource "null_resource" "aws_lz_register_tgw_globalnet" {
 
-  count = length(var.globalnet_id) > 0 ? 1 : 0
+  #count = length(var.globalnet_id) > 0 ? 1 : 0
 
   provisioner "local-exec" {
     command = "aws networkmanager register-transit-gateway --global-network-id ${var.globalnet_id} --transit-gateway-arn ${var.tgw_arn}"
