fd4b24094381039697b0baa17d7a404707919af9
csanchez <cesar.sanchez@gft.com>
2020-04-21 09:00:18 -0600

    Global Net output

--- terraform/modules/transit-gateway/tgw-net-manager/output.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/output.tf
@@ -4,7 +4,7 @@ data "template_file" "log_name" {
 
 data "local_file" "create-globalnet" {
   filename = data.template_file.log_name.rendered
-  depends_on = ["null_resource.aws_lz_ena_net_manager"]
+  depends_on = [null_resource.aws_lz_ena_net_manager]
 }
 
 output "aws-cli-output" {
