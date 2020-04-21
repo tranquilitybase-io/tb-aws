33cc0ec61817c4599d395b4c06ec8aa14e02caaf
csanchez <cesar.sanchez@gft.com>
2020-04-21 10:40:31 -0600

    Creating Global Network

--- terraform/modules/transit-gateway/tgw-net-manager/main.tf
+++ terraform/modules/transit-gateway/tgw-net-manager/main.tf
@@ -4,13 +4,9 @@
 * Usage: Generic Terraform module to Enable TGW Network Manager Service.
 */
 
-locals {
-  gn_tags = merge({"Name" = format("%s", var.global_network_name)}, var.tags)
-}
-
 resource "null_resource" "aws_lz_ena_net_manager" {
     provisioner "local-exec" {
-    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags '[${local.gn_tags}]' > ${data.template_file.log_name.rendered}"
+    command = "aws networkmanager create-global-network --description '${var.global_network_description}' --tags Key=Name,Value=${var.global_network_name} > ${data.template_file.log_name.rendered}"
   }
 }
 
