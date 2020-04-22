2b7d31377496348d6cb7b13f000838378dc12f3e
Deiver Kiel <deiver.kiel@gft.com>
2020-04-22 12:32:34 -0600

    test jq

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -50,11 +50,9 @@ aws sts get-caller-identity
 echo "------------------------get-caller-identity-----Network AdminOU-------------------"
 #aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
 temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
-##
-jq --version
-#export AWS_ACCESS_KEY_ID=$(echo temp_json)
-#export AWS_SECRET_ACCESS_KEY=$(echo temp_json | jq -r ".Credentials | .SecretAccessKey")
-#export AWS_SESSION_TOKEN=$(echo temp_json | jq -r ".Credentials | .SessionToken")
+export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
+export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
 #aws sts get-caller-identity
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
