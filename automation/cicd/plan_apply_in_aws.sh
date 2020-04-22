10656f14cac0adbb8e343a7d396a9d876d91172d
Deiver Kiel <deiver.kiel@gft.com>
2020-04-22 12:18:33 -0600

    test terra identity

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -48,11 +48,11 @@ echo "------------------------get-caller-identity-----TERRAFORM-----------------
 aws sts get-caller-identity
 
 echo "------------------------get-caller-identity-----Network AdminOU-------------------"
-temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
-export AWS_ACCESS_KEY_ID=$(echo temp_json | jq -r ".Credentials | .AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(echo temp_json | jq -r ".Credentials | .SecretAccessKey")
-export AWS_SESSION_TOKEN=$(echo temp_json | jq -r ".Credentials | .SessionToken")
-aws sts get-caller-identity
+#temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
+#export AWS_ACCESS_KEY_ID=$(echo temp_json | jq -r ".Credentials | .AccessKeyId")
+#export AWS_SECRET_ACCESS_KEY=$(echo temp_json | jq -r ".Credentials | .SecretAccessKey")
+#export AWS_SESSION_TOKEN=$(echo temp_json | jq -r ".Credentials | .SessionToken")
+#aws sts get-caller-identity
 #This scripts generates the Guardduty instances in all accounts and all regions
 #python3 ${AUTOMATION_SCRIPTS}/guardduty.py
 
