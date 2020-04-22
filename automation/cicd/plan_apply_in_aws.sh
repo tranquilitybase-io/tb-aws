83b74ed5345f6bc31cf81f2108c81241af7733d0
Deiver Kiel <deiver.kiel@gft.com>
2020-04-22 12:19:39 -0600

    assume network

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -48,6 +48,7 @@ echo "------------------------get-caller-identity-----TERRAFORM-----------------
 aws sts get-caller-identity
 
 echo "------------------------get-caller-identity-----Network AdminOU-------------------"
+aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
 #temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
 #export AWS_ACCESS_KEY_ID=$(echo temp_json | jq -r ".Credentials | .AccessKeyId")
 #export AWS_SECRET_ACCESS_KEY=$(echo temp_json | jq -r ".Credentials | .SecretAccessKey")
