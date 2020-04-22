ea8258a9d714a8e582cfb9fa9e3a1fce555b155f
iota <ignacio.tissera@gft.com>
2020-04-22 14:41:51 -0600

    New task on Gitlab for deploying k8s

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -36,23 +36,6 @@ ssh-keygen -t rsa -b 2048 -N "" -f temp--${TF_VAR_env_generation_date}.key > /de
 export TF_VAR_env_deployment_key=$(cat temp--${TF_VAR_env_generation_date}.key.pub)
 
 
-echo "------------------------get-caller-identity-----TERRAFORM-------------------------"
-#aws sts get-caller-identity
-
-echo "------------------------get-caller-identity-----Network AdminOU-------------------"
-##aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
-#temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
-#export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
-#export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
-#export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
-#aws sts get-caller-identity
-
-echo "------------------------Validate kubeconfig and kubectl---------------------------"
-#aws eks --region ${DEV_region} update-kubeconfig --name awslz_eks_ingress_cluster
-#kubectl get svc
-
-echo "----------------------------------------------------------------------------------"
-#more ~/.kube/config
 
 echo "------------------------TERRAFORM INIT--------------------------------------------"
 cd ${TERRAFORM_PATH}
