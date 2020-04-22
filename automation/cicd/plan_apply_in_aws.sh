e0d19a56edc99a1ff9f8317d3e04a0501470a0ca
iota <ignacio.tissera@gft.com>
2020-04-22 13:06:53 -0600

    Deploy to network account k8s

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -37,28 +37,28 @@ export TF_VAR_env_deployment_key=$(cat temp--${TF_VAR_env_generation_date}.key.p
 
 
 echo "------------------------get-caller-identity-----TERRAFORM-------------------------"
-aws sts get-caller-identity
+#aws sts get-caller-identity
 
 echo "------------------------get-caller-identity-----Network AdminOU-------------------"
-#aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
-temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
-export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
-export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
-aws sts get-caller-identity
+##aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
+#temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
+#export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
+#export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
+#export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
+#aws sts get-caller-identity
 
 echo "------------------------Validate kubeconfig and kubectl---------------------------"
-aws eks --region ${DEV_region} update-kubeconfig --name awslz_eks_ingress_cluster
-kubectl get svc
+#aws eks --region ${DEV_region} update-kubeconfig --name awslz_eks_ingress_cluster
+#kubectl get svc
 
 echo "----------------------------------------------------------------------------------"
-more ~/.kube/config
+#more ~/.kube/config
 
 echo "------------------------TERRAFORM INIT--------------------------------------------"
 cd ${TERRAFORM_PATH}
-export AWS_ACCESS_KEY_ID=${access_key}
-export AWS_SECRET_ACCESS_KEY=${secret_key}
-export AWS_DEFAULT_REGION=${DEV_region}
+#export AWS_ACCESS_KEY_ID=${access_key}
+#export AWS_SECRET_ACCESS_KEY=${secret_key}
+#export AWS_DEFAULT_REGION=${DEV_region}
 terraform init
 echo "------------------------TERRAFORM APPLY-------------------------------------------"
 #TF_LOG=DEBUG terraform apply -refresh=true -auto-approve
