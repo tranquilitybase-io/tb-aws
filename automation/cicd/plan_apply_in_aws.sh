09f7ced6e07504c900a1f46f01b82a76e600192e
Deiver Kiel <deiver.kiel@gft.com>
2020-04-22 12:22:36 -0600

    test key var

--- automation/cicd/plan_apply_in_aws.sh
+++ automation/cicd/plan_apply_in_aws.sh
@@ -50,8 +50,8 @@ aws sts get-caller-identity
 echo "------------------------get-caller-identity-----Network AdminOU-------------------"
 #aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network
 temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::615513573213:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-network)
-echo $temp_json
-#export AWS_ACCESS_KEY_ID=$(echo temp_json | jq -r ".Credentials | .AccessKeyId")
+
+export AWS_ACCESS_KEY_ID=$(echo temp_json | jq -r ".Credentials | .AccessKeyId")
 #export AWS_SECRET_ACCESS_KEY=$(echo temp_json | jq -r ".Credentials | .SecretAccessKey")
 #export AWS_SESSION_TOKEN=$(echo temp_json | jq -r ".Credentials | .SessionToken")
 #aws sts get-caller-identity
