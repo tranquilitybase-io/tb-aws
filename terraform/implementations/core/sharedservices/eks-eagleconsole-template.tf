7ec593b022bf664dec8ac274a5277351793c4aa3
Deiver Kiel <deiver.kiel@gft.com>
2020-04-24 12:43:19 -0600

    Deploy ec eks cluster in shared account

--- terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
+++ terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
@@ -2,17 +2,17 @@
 module "aws_lz_eks_eagleconsole_cluster" {
   source = "./modules/eks"
   providers = {
-    aws = aws.network-account
+    aws = aws.sharedservices-account
   }
 
   eks_cluster_name          = var.ec_eks_cluster_name
   eks_iam_role_name         = var.ec_eks_role_name
-  subnets                   = module.aws_lz_ingress_vpc.private_subnets
+  subnets                   = module.vpc_shared_services.private_subnets
   # eks_user_policy         = data.aws_iam_policy_document.aws_lz_eks_cluster_sharedservices.json
 
   node_group_name           = var.ec_eks_node_group_name
   node_group_role_name      = var.ec_eks_node_group_role_name
-  node_group_subnets        = module.aws_lz_ingress_vpc.private_subnets
-  node_group_instance_types = ["t3.small"]
+  node_group_subnets        = module.vpc_shared_services.private_subnets
+  node_group_instance_types = ec_eks_node_group_instance_types
 }
 # END EKS cluster
