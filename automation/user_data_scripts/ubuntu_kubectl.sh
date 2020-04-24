46372ae6b202b3ac24bcb507d52d26d793472d91
Deiver Kiel <deiver.kiel@gft.com>
2020-04-24 16:22:33 -0600

    Deploy instance kubectl

---
+++ automation/user_data_scripts/ubuntu_kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+apt update -y
+
+
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --short --client
