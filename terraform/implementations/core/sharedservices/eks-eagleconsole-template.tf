74e060c126029030b74142c1a4ef26dd3d88ecd0
Deiver Kiel <deiver.kiel@gft.com>
2020-04-26 23:26:35 -0600

    test get certificate

--- terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
+++ terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
@@ -35,7 +35,8 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+//  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_id.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
   version                = "~> 1.11"
