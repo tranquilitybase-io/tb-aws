d9af12550101b120b71ee6b79f176a89340e41dd
Deiver Kiel <deiver.kiel@gft.com>
2020-04-26 23:33:17 -0600

    test get data

--- terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
+++ terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
@@ -34,7 +34,8 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 */
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
+//  host                   = data.aws_eks_cluster.cluster.endpoint
+  host                   = module.eks.cluster_id.endpoint
 //  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   cluster_ca_certificate = base64decode(module.eks.cluster_id.certificate_authority.0.data)
 //  token                  = data.aws_eks_cluster_auth.cluster.token
