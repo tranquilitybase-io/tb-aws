779eeadeb67ec86b69b3fafebea9896ac48bd52e
Deiver Kiel <deiver.kiel@gft.com>
2020-04-26 21:35:41 -0600

    test providers in share

--- terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
+++ terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
@@ -18,94 +18,14 @@ module "aws_lz_eks_eagleconsole_cluster" {
 # END EKS cluster
 */
 
-provider "random" {
-  version = "~> 2.1"
-}
+module "aws_eks_sg" {
+  source      = "./modules/eks-2"
 
-
-provider "template" {
-  version = "~> 2.1"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
-
-//data "aws_availability_zones" "available" {
-//}
-
-locals {
-  cluster_name = "ec-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-resource "aws_security_group" "worker_group_mgmt_one" {
   providers = {
     aws = aws.sharedservices-account
   }
   
-  name_prefix = "worker_group_mgmt_one"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
-
-resource "aws_security_group" "worker_group_mgmt_two" {
-
-  name_prefix = "worker_group_mgmt_two"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
-}
-
-resource "aws_security_group" "all_worker_mgmt" {
-
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
-  }
+  eks_vpc_id = module.vpc.vpc_id
 }
 
 module "vpc" {
