a29d6e42081e9242d690637ec33c35d27b2a6895
Deiver Kiel <deiver.kiel@gft.com>
2020-04-27 02:07:33 -0600

    validate cluster

--- automation/cicd/aws_post_deploy.sh
+++ automation/cicd/aws_post_deploy.sh
@@ -39,8 +39,13 @@ function share_session(){
 function check_eks_cluster() {
     echo "-----------------------Clusters List------------------------------------"
     aws eks list-clusters
-    aws eks list-clusters | jq -r '.clusters | contains(["awslz_eks_eagleconsole"])'
-    echo $?
+    cluster_exists=$(aws eks list-clusters | jq -r '.clusters | contains(["awslz_eks_eagleconsole"])')
+    if ${cluster_exists} -eq "true" 
+    then
+        echo "cluster exists"
+    else
+        echo "cluster does not exists"
+    fi
 }
 
 function run_kubeconfig() {
