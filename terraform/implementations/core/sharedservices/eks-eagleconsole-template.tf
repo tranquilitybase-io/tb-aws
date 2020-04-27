98eed56184d0f277dcdac44a7df7409c489bd37a
Deiver Kiel <deiver.kiel@gft.com>
2020-04-26 23:00:43 -0600

    test provider

--- terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
+++ terraform/implementations/core/sharedservices/eks-eagleconsole-template.tf
@@ -21,6 +21,18 @@ provider "random" {
   version = "~> 2.1"
 }
 
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
 resource "random_string" "suffix" {
   length  = 8
   special = false
--- terraform/modules/eks-2/main.tf
+++ terraform/modules/eks-2/main.tf
@@ -1,17 +1,5 @@
 
-provider "template" {
-  version = "~> 2.1"
-}
 
-
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
 
 //data "aws_availability_zones" "available" {
 //}
