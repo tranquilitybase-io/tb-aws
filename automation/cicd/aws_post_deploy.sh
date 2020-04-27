ce29e3e321fa1116cd66beb130dda70b7f006495
Deiver Kiel <deiver.kiel@gft.com>
2020-04-27 01:38:31 -0600

    run_post deploy

--- automation/cicd/aws_post_deploy.sh
+++ automation/cicd/aws_post_deploy.sh
@@ -1,46 +1,68 @@
 #!/bin/bash
 
-sharedservices_eks_cluster="awslz_eks_eagleconsole"
-network_eks_cluster=""
-
-
-echo "------------------------AWS Environment VAriables -----------------------------------------------"
-export AWS_ACCESS_KEY_ID=${access_key}
-export AWS_SECRET_ACCESS_KEY=${secret_key}
-export AWS_DEFAULT_REGION=${DEV_region}
-
 # Paths
 MAIN_PATH=$(pwd)
 AUTOMATION_SCRIPTS="${MAIN_PATH}/automation/cicd"
 TERRAFORM_PATH="${MAIN_PATH}/terraform"
 K8S_PATH="${MAIN_PATH}/automation/k8s"
 
-echo "------------------------AWS CLI-----------------------------------------------"
-aws --version
-aws configure set aws_access_key_id ${access_key}
-aws configure set aws_secret_access_key ${secret_key}
-aws configure set default.region ${DEV_region}
-echo "------------------------get-caller-identity-----TERRAFORM-------------------------"
-aws sts get-caller-identity
+# Variables
+sharedservices_eks_cluster="awslz_eks_eagleconsole"
+network_eks_cluster=""
 
-echo "------------------------get-caller-identity-----Shared Services AdminOU-------------------"
-temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::000516684594:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-sharedservices)
-export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
-export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
-aws sts get-caller-identity
+function init_config() {
+    echo "------------------------AWS Environment VAriables -----------------------------------------------"
+    export AWS_ACCESS_KEY_ID=${access_key}
+    export AWS_SECRET_ACCESS_KEY=${secret_key}
+    export AWS_DEFAULT_REGION=${DEV_region}
+}
 
+function aws_config(){
+    echo "------------------------AWS CLI-----------------------------------------------"
+    aws --version
+    aws configure set aws_access_key_id ${access_key}
+    aws configure set aws_secret_access_key ${secret_key}
+    aws configure set default.region ${DEV_region}
+    echo "------------------------get-caller-identity-----TERRAFORM-------------------------"
+    aws sts get-caller-identity
+}
 
-echo "------------------------Validate kubeconfig and kubectl---------------------------"
-aws eks --region ${DEV_region} update-kubeconfig --name awslz_eks_eagleconsole
-kubectl get svc
-kubectl apply -f ${K8S_PATH}/eagle-console/eagle-console-deployment.yaml
-kubectl apply -f ${K8S_PATH}/eagle-console/houston-deployment.yaml
-kubectl apply -f ${K8S_PATH}/eagle-console/mysql-deployment.yaml
-sleep 1m
-kubectl get pods
-echo "------------------------Services deployments--------------------------------------"
-kubectl get svc
-kubectl apply -f ${K8S_PATH}/eagle-console/eagle-console-service.yaml
-sleep 1m
-kubectl get svc
+function share_session(){
+    echo "------------------------get-caller-identity-----Shared Services AdminOU-------------------"
+    temp_json=$(aws sts assume-role --role-arn "arn:aws:iam::000516684594:role/AWSLZCoreOUAdminRole" --role-session-name AWSCLI-Session-sharedservices)
+    export AWS_ACCESS_KEY_ID=$(echo ${temp_json} | jq -r ".Credentials | .AccessKeyId")
+    export AWS_SECRET_ACCESS_KEY=$(echo ${temp_json} | jq -r ".Credentials | .SecretAccessKey")
+    export AWS_SESSION_TOKEN=$(echo ${temp_json} | jq -r ".Credentials | .SessionToken")
+    aws sts get-caller-identity
+}
+
+function check_eks_cluster() {
+    aws eks list-clusters
+}
+
+function run_kubeconfig() {
+    echo "------------------------Kubeconfig and kubectl---------------------------"
+    aws eks --region ${DEV_region} update-kubeconfig --name awslz_eks_eagleconsole
+    kubectl get svc
+}
+
+function deploy_pods {
+    kubectl apply -f ${K8S_PATH}/eagle-console/eagle-console-deployment.yaml
+    kubectl apply -f ${K8S_PATH}/eagle-console/houston-deployment.yaml
+    kubectl apply -f ${K8S_PATH}/eagle-console/mysql-deployment.yaml
+    sleep 1m
+    kubectl get pods
+}
+
+function deploy_services() {
+    echo "------------------------Services deployments--------------------------------------"
+    kubectl get svc
+    kubectl apply -f ${K8S_PATH}/eagle-console/eagle-console-service.yaml
+    sleep 1m
+    kubectl get svc
+}
+
+init_config
+aws_config
+share_session
+check_eks_cluster
